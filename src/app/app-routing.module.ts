5167973b6b4ef95a0728508cb81b79c406215b3d
Lorenz Detterbeck <lorenz.detterbeck@mni.thm.de>
2018-03-05 11:43:34 +0100

    remove comments as mentioned in merge request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorLandingPageComponent } from './error-landing-page/error-landing-page.component';
-// import { GewünschstesModul }  from './blabla/wunschmodul';
 
 
 const routes: Routes = [
-//  { path: '', redirectTo: '/wumod', pathMatch: 'full' }, //default route
-//  { path: 'wumod', component: GewünschstesModul },
 { path: 'errorpage', component: ErrorLandingPageComponent }
 ];
 
