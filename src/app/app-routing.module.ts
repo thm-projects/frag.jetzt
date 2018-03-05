c0bcd15b368aefdeee4e305e8c7738c090404622
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-03-05 17:03:39 +0100

    Set the login-screen-page as start in the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 const routes: Routes = [
-{ path: 'errorpage', component: PageNotFoundComponent }
+{ path: 'errorpage', component: PageNotFoundComponent },
+  {path: 'home', component: LoginScreenComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 
