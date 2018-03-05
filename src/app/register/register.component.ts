f91bb67d51b5198a780c980c7526d8193c9d62ed
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-05 11:53:40 +0100

    Add input fields and buttons

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +6,7 @@ import { Component, NgModule, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  check = 0;
 
   constructor() {
   }
@@ -13,4 +14,12 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  save(email: string, password1: string, password2: string): void {
+   // console.log(email, password1, password2);
+    if (password1 === password2) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
 }
