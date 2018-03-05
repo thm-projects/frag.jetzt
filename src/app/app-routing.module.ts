5ebaf75c930bb9b924ea795d7fb97cc7dc7d6fe5
Lorenz Detterbeck <lorenz.detterbeck@mni.thm.de>
2018-03-05 10:48:52 +0100

    change indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { GewünschstesModul }  from './blabla/wunschmodul';
 
