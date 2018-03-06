75f0e02bde3d68acb7a49c03db7aec1c828dbba5
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-03-06 14:48:18 +0100

    Fixed code syles finally

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable()
