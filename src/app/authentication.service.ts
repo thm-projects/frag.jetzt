35d231f9850ddb4eeab1c4f11996ecad40e70bec
David Donges <david.donges@mni.thm.de>
2018-03-06 14:16:03 +0100

    Add dummy logout function to authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -22,6 +22,10 @@ export class AuthenticationService {
     return of(true);
   }
 
+  logout() {
+    this.mockUser = null;
+  }
+
   getUser(): Observable<User> {
     return of(this.mockUser);
   }
