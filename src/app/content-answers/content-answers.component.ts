215402d2099c656ad10c24de2e4d3dc0cadd97c2
Lorenz Detterbeck <lorenz.detterbeck@gmail.com>
2018-03-06 12:01:48 +0100

    Add content card

--- src/app/content-answers/content-answers.component.ts
+++ src/app/content-answers/content-answers.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentAnswersComponent implements OnInit {
 
-  checked = false;
-  indeterminate = false;
   align = 'start';
-  disabled = false;
 
   constructor() { }
 
