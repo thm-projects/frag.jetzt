15ff4de1c9342181cbc77f503b42c92e8020d403
Hagen <hagen.dressler@mni.thm.de>
2018-03-06 15:50:58 +0100

    Rename component comment-creator to comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { JoinRoomComponent } from './join-room/join-room.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
-import { CommentCreatorComponent } from './comment-creator/comment-creator.component';
+import { CommentComponent } from './comment/comment.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -77,7 +77,7 @@ import { CreateCommentComponent } from './create-comment/create-comment.componen
     RoomListComponent,
     CreatorHomeScreenComponent,
     CreateCommentComponent,
-    CommentCreatorComponent
+    CommentComponent
   ],
   entryComponents: [
     RegisterComponent,
--- src/app/comment/comment.component.spec.ts
+++ src/app/comment/comment.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CommentCreatorComponent } from './comment-creator.component';
+import { CommentComponent } from './comment.component';
 
-describe('CommentCreatorComponent', () => {
-  let component: CommentCreatorComponent;
-  let fixture: ComponentFixture<CommentCreatorComponent>;
+describe('CommentComponent', () => {
+  let component: CommentComponent;
+  let fixture: ComponentFixture<CommentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CommentCreatorComponent ]
+      declarations: [ CommentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CommentCreatorComponent);
+    fixture = TestBed.createComponent(CommentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
