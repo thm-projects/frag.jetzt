e1f24fda84b6c7816bae7bee7ea981b266d1ceaf
Heinrich Marks <heinrich.marks@mni.thm.de>
2018-03-06 14:40:14 +0100

    Add content class

---
+++ src/app/content.ts
@@ -0,0 +1,20 @@
+enum Format {
+    CHOICE,
+    BINARY,
+    SCALE,
+    NUMBER,
+    TEXT,
+    GRID
+  }
+  
+export class Content {
+    id: string;
+    revision: string;
+    roomId: string;
+    subject: string;
+    body: string;
+    round: number;
+    format: Format;
+    formatAttributes: Map<string, string>;
+  }
+
