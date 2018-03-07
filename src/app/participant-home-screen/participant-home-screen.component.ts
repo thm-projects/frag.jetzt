7009d6cb8bba2da9552298f8f6561109a58cd18c
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-07 13:38:01 +0100

    Add participant-home-screen component. HTML: included join-room and room-list components

---
+++ src/app/participant-home-screen/participant-home-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participant-home-screen',
+  templateUrl: './participant-home-screen.component.html',
+  styleUrls: ['./participant-home-screen.component.scss']
+})
+export class ParticipantHomeScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
