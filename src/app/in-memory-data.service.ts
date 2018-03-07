eccdfd23d0728a05e239f65de181cfb8352c1b56
David Donges <david.donges@mni.thm.de>
2018-03-07 18:02:07 +0100

    Overwrite InMemoryDbService's genId function

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,26 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
+  /**
+   * Gets called by the in memory db for generating ids for newly inserted entities.
+   * As the default genId function is only capable of generating numeric ids but we're
+   * using string ids (as per api) we're overwriting it relying on a stupid fallback for strings:
+   * append a '1' to the id of the last element
+   * @param {any[]} collection
+   * @param {string} collectionName
+   * @returns {any}
+   */
+  genId(collection: any[], collectionName: string): any {
+    // Get the id of the last element
+    const lastElementId = collection[collection.length - 1].id;
+    if (isNaN(lastElementId)) {
+      // Append '1' to the string id when the id is not convertable to a number
+      return lastElementId + '1';
+    }
+    // Convert id to a number, add 1 and convert back to string
+    return (+lastElementId + 1).toString();
+  }
+
   createDb() {
     const rooms = [
       {
