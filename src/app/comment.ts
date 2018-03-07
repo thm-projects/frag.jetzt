396f2db3ec3bea43259f165bbc71aac9693ccafa
Hagen <hagen.dressler@mni.thm.de>
2018-03-08 00:33:23 +0100

    Remove constructor from model comment

--- src/app/comment.ts
+++ src/app/comment.ts
@@ -6,14 +6,4 @@ export class Comment {
   body: string;
   read: boolean;
   creationTimestamp: Date;
-
-  constructor(id: string, roomId: string, subject: string, body: string, creationTimestamp: Date) {
-    this.id = id;
-    this.roomId = roomId;
-    this.revision = null;
-    this.subject = subject;
-    this.body = body;
-    this.read = false;
-    this.creationTimestamp = creationTimestamp;
-  }
 }
