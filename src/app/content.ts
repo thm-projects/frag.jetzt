dca18435300a30807a6e6f27bb8e65ee550017e8
Lorenz Detterbeck <lorenz.detterbeck@gmail.com>
2018-03-07 11:14:46 +0100

    Fix indentation

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,19 +1,19 @@
 enum Format {
-    CHOICE,
-    BINARY,
-    SCALE,
-    NUMBER,
-    TEXT,
-    GRID
+  CHOICE,
+  BINARY,
+  SCALE,
+  NUMBER,
+  TEXT,
+  GRID
   }
 export class Content {
-    id: string;
-    revision: string;
-    roomId: string;
-    subject: string;
-    body: string;
-    round: number;
-    format: Format;
-    formatAttributes: Map<string, string>;
+  id: string;
+  revision: string;
+  roomId: string;
+  subject: string;
+  body: string;
+  round: number;
+  format: Format;
+  formatAttributes: Map<string, string>;
   }
 
