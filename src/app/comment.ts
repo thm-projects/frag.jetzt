5fe5f9aab86ed650ba5f5ff7a8688ce033892e80
Hagen <hagen.dressler@mni.thm.de>
2018-03-07 13:56:52 +0100

    Added simple constructor to model comment

--- src/app/comment.ts
+++ src/app/comment.ts
@@ -6,4 +6,14 @@ export class Comment {
   body: string;
   read: boolean;
   creationTimestamp: Date;
+
+  constructor(id: string, roomId: string, subject: string, body: string, creationTimestamp: Date) {
+    this.id = id;
+    this.roomId = roomId;
+    this.revision = null;
+    this.subject = subject;
+    this.body = body;
+    this.read = false;
+    this.creationTimestamp = creationTimestamp;
+  }
 }
