eab26b6d7205121be213710686395880c7c1d426
David Donges <david.donges@mni.thm.de>
2018-03-07 12:14:15 +0100

    Format code

--- src/app/room-creation/room-creation.component.ts
+++ src/app/room-creation/room-creation.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class RoomCreationComponent implements OnInit {
   longName: string;
   shortName: string;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
