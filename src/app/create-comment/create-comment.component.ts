41773de1ec957b1a3ebf2693b0cb54c5a1860f1f
Hagen <hagen.dressler@mni.thm.de>
2018-03-07 10:42:36 +0100

    Add component create-comment

---
+++ src/app/create-comment/create-comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-comment',
+  templateUrl: './create-comment.component.html',
+  styleUrls: ['./create-comment.component.scss']
+})
+export class CreateCommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
