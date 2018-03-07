83bba21ed4df3318a485add067b5e32249171efe
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-07 13:46:26 +0100

    Add route to participant-home-screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { RoomComponent } from './room/room.component';
 import { CreatorHomeScreenComponent } from './creator-home-screen/creator-home-screen.component';
+import { ParticipantHomeScreenComponent } from './participant-home-screen/participant-home-screen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: LoginScreenComponent },
   { path: 'creator', component: CreatorHomeScreenComponent },
   { path: 'room/:roomId', component: RoomComponent },
+  { path: 'participant', component: ParticipantHomeScreenComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
