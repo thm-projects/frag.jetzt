4bd1d51afc8b1e21fa8bda4f75485df117566763
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-03-08 23:39:56 +0100

    Refactor participant room component (view) Add some more demo room data Use Material's Card for room view Add some more buttons Add some simple stylings

--- src/app/participant-room/participant-room.component.ts
+++ src/app/participant-room/participant-room.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participant-room.component.scss']
 })
 export class ParticipantRoomComponent implements OnInit {
-  roomId = '1';
+
+  roomId = '12 34 56 78';
+  roomName = 'Test Room';
+  roomDescription = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore ' +
+    'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea ' +
+    'takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ' +
+    'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
+    'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
   constructor() {
   }
 
