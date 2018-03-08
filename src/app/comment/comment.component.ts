f7f71e6fb40d4b67b7164912db4505a88506ede4
Hagen <hagen.dressler@mni.thm.de>
2018-03-08 11:00:54 +0100

    Add variable to component comment

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Comment } from '../comment';
 
 @Component({
   selector: 'app-comment',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  comments: Comment[];
 
   constructor() { }
 
