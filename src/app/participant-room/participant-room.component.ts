e4aca90a0338d5bac2cb710b783ee5771fc3ab7e
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-08 14:42:39 +0100

    Create participant-room component with simple button-elements in HTML view

---
+++ src/app/participant-room/participant-room.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participant-room',
+  templateUrl: './participant-room.component.html',
+  styleUrls: ['./participant-room.component.scss']
+})
+export class ParticipantRoomComponent implements OnInit {
+  roomId = '1';
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
