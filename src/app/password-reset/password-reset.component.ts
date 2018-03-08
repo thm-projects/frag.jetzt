3ab62ff9213669bca8602080e38c1617bfa1d245
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-03-08 13:34:19 +0100

    Implement password reset logic Use angular-flex for form Implement FormControl and Matcher

--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { AuthenticationService } from '../authentication.service';
+import { NotificationService } from '../notification.service';
+import { RegisterComponent } from '../register/register.component';
+
+export class PasswordResetErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return (control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-password-reset',
@@ -7,9 +19,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordResetComponent implements OnInit {
 
-  constructor() { }
+
+  usernameFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new PasswordResetErrorStateMatcher();
+
+  constructor(public authenticationService: AuthenticationService,
+              public notificationService: NotificationService,
+              public dialogRef: MatDialogRef<RegisterComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
 
   ngOnInit() {
+  }
+
+  resetPassword(username: string): void {
+    username = username.trim();
+
+    if (username) {
+      this.authenticationService.resetPassword(username).subscribe(result => {
+        if (result) {
+          this.notificationService.show('Password was reset. Please check your mail!');
+          this.dialogRef.close();
+        } else {
+          this.notificationService.show('Could not reset your password. Is your email address correct?');
+        }
+      });
+    } else {
+      this.notificationService.show('Email address can not be empty.');
+    }
   }
 
 }
