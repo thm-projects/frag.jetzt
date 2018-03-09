0f879a3925ac439d8e2c608d54074d649b161930
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-03-09 12:02:54 +0100

    Remove useless test code

--- src/app/participant-room/participant-room.component.ts
+++ src/app/participant-room/participant-room.component.ts
@@ -13,14 +13,6 @@ export class ParticipantRoomComponent implements OnInit {
 
   room: Room;
 
-  roomId = '12 34 56 78';
-  roomName = 'Test Room';
-  roomDescription = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore ' +
-    'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea ' +
-    'takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod ' +
-    'tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea ' +
-    'rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
-
   constructor(private location: Location,
               private roomService: RoomService,
               private route: ActivatedRoute) {
