7afe351b6973dd7b5dfb36f7f996a6db18badd65
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-03-11 13:09:19 +0100

    Adjist join-room so that you can't enter a room which is not existing

--- src/app/join-room/join-room.component.ts
+++ src/app/join-room/join-room.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+import { Router } from '@angular/router';
+import { NotificationService } from '../notification.service';
 
 @Component({
   selector: 'app-join-room',
   templateUrl: './join-room.component.html',
   styleUrls: ['./join-room.component.scss']
 })
+
 export class JoinRoomComponent implements OnInit {
 
-  constructor() { }
+  room: Room;
+  isExisting = true;
+  noInput = false;
 
-  ngOnInit() {
+  constructor(private roomService: RoomService,
+              private router: Router,
+              private notificationService: NotificationService) {
   }
 
+  ngOnInit() {
+
+  }
+
+  joinRoom(id: string): void {
+    if (id) {
+        this.roomService.getRoom(id)
+          .subscribe( room => {
+            this.room = room;
+            if (!room) {
+              this.isExisting = false;
+              this.noInput = false;
+              this.notificationService.show('Invalid room-id.');
+            } else {
+              this.router.navigate([`/participant/room/${this.room.id}`], { relativeTo: this.route });
+            }
+          });
+    } else {
+      this.noInput = true;
+      this.isExisting = true;
+      this.notificationService.show('No room-id entered.');
+    }
+  }
 }
