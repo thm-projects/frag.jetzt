28642f80989c60f92c44b6fbfe3d452a22c70bc7
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-12 13:40:44 +0100

    Add room-deletion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -72,6 +72,7 @@ import { ContentListComponent } from './content-list/content-list.component';
 import { ContentService } from './content.service';
 import { ContentAnswersListComponent } from './content-answers-list/content-answers-list.component';
 import { ContentAnswerService } from './content-answer.service';
+import { RoomDeletionComponent } from './room-deletion/room-deletion.component';
 
 @NgModule({
   declarations: [
@@ -97,7 +98,8 @@ import { ContentAnswerService } from './content-answer.service';
     CreatorRoomComponent,
     ContentDetailComponent,
     ContentListComponent,
-    ContentAnswersListComponent
+    ContentAnswersListComponent,
+    RoomDeletionComponent
   ],
   entryComponents: [
     RegisterComponent,
---
+++ src/app/room-deletion/room-deletion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-deletion',
+  templateUrl: './room-deletion.component.html',
+  styleUrls: ['./room-deletion.component.scss']
+})
+export class RoomDeletionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
