6ad188624ee3148e7c4e3063a90220d8dd0106b6
Lorenz Detterbeck <lorenz.detterbeck@gmail.com>
2018-03-12 15:38:10 +0100

    Add todo for tomorrow

--- src/app/content-answers-list/content-answers-list.component.ts
+++ src/app/content-answers-list/content-answers-list.component.ts
@@ -3,6 +3,7 @@ import { ContentAnswerService } from '../content-answer.service';
 import { AnswerText } from '../answer-text';
 import { ContentDetailComponent } from '../content-detail/content-detail.component';
 import { Content } from '../content';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content-answers-list',
@@ -12,13 +13,17 @@ import { Content } from '../content';
 export class ContentAnswersListComponent implements OnInit {
   textAnswers: AnswerText[];
   content: Content[];
+  filteredConrtent: Content[];
 
   constructor(private contentAnswerService: ContentAnswerService,
-              private contentDetailComponent: ContentDetailComponent) { }
+              private contentDetailComponent: ContentDetailComponent,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getAnswerTexts();
-    // this.contentDetailComponent.getContent(???);
+    this.route.params.subscribe(params => {
+      this.contentDetailComponent.getContent(params['id']); // todo: filtered content befüllen
+    });
   }
 
   getAnswerTexts(): void {
