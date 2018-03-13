726a3ccdbcec1957040b241131c87ad7c255f05f
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-03-13 12:53:58 +0100

    Refactor login component to use new features

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,8 @@ import { NotificationService } from '../notification.service';
 import { ErrorStateMatcher } from '@angular/material';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { UserRole } from '../user-roles.enum';
+import { ClientAuthentication } from '../client-authentication';
+import { User } from '../user';
 
 export class LoginErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -43,7 +45,7 @@ export class LoginComponent implements OnInit {
 
     if (!this.usernameFormControl.hasError('required') && !this.usernameFormControl.hasError('email') &&
       !this.passwordFormControl.hasError('required')) {
-      this.authenticationService.login(username, password, this.role).subscribe(loginSuccessful => this.checkLogin(loginSuccessful));
+      this.authenticationService.login(username, password).subscribe(loginSuccessful => this.checkLogin(loginSuccessful));
     } else {
       this.notificationService.show('Please fit the requirements shown above.');
     }
@@ -53,8 +55,15 @@ export class LoginComponent implements OnInit {
     this.authenticationService.guestLogin().subscribe(loginSuccessful => this.checkLogin(loginSuccessful));
   }
 
-  private checkLogin(loginSuccessful: boolean) {
+  private checkLogin(loginSuccessful: ClientAuthentication) {
     if (loginSuccessful) {
+      const user: User = new User(
+        loginSuccessful.userId,
+        loginSuccessful.loginId,
+        loginSuccessful.authProvider,
+        loginSuccessful.token,
+        this.role);
+      this.authenticationService.setUser(user);
       this.notificationService.show('Login successful!');
       if (this.role === UserRole.CREATOR) {
         this.router.navigate(['creator']);
