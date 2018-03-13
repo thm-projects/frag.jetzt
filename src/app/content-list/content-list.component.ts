048b16f432161cb4c9bba2da54c58d3cede101d6
Heinrich Marks <heinrich.marks@mni.thm.de>
2018-03-13 15:42:00 +0100

    Adjust logic to show respective content list items only now

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../content.service';
 import { Content } from '../content';
+import { ActivatedRoute } from '@angular/router';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-content-list',
@@ -10,14 +12,27 @@ import { Content } from '../content';
 export class ContentListComponent implements OnInit {
   contents: Content[];
 
-  constructor(private contentService: ContentService) { }
+  constructor(
+    private contentService: ContentService,
+    private route: ActivatedRoute,
+    private roomService: RoomService,
+  ) { }
 
   ngOnInit() {
-    this.getContents();
+    this.route.params.subscribe(params => {
+      this.getRoom(params['roomId']);
+    });
   }
 
-  getContents(): void {
-    this.contentService.getContents()
+  getRoom(id: string): void {
+    this.roomService.getRoom(id).subscribe(
+      params => {
+        this.getContents(params['id']);
+      });
+  }
+
+  getContents(roomId: string): void {
+    this.contentService.getContents(roomId)
     .subscribe(contents => {
       this.contents = contents;
     });
