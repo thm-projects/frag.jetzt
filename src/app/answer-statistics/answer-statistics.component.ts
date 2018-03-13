e6483404ae7c30df0dc418239cbf5a2db14e4456
Hagen <hagen.dressler@mni.thm.de>
2018-03-13 14:12:50 +0100

    Add components answer-statistics

---
+++ src/app/answer-statistics/answer-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-statistics',
+  templateUrl: './answer-statistics.component.html',
+  styleUrls: ['./answer-statistics.component.scss']
+})
+export class AnswerStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
