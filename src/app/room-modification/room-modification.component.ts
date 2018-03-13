83a9985f7be7edb16b9a18c8a339a1e971cb412d
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-13 13:45:35 +0100

    Add room-modification component

---
+++ src/app/room-modification/room-modification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-modification',
+  templateUrl: './room-modification.component.html',
+  styleUrls: ['./room-modification.component.scss']
+})
+export class RoomModificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
