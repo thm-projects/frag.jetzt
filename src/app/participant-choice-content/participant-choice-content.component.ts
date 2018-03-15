91f4e8f8b418e2c2b673144b4a3c85f37eee0f1a
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-15 11:05:47 +0100

    Implement first view and fake data

--- src/app/participant-choice-content/participant-choice-content.component.ts
+++ src/app/participant-choice-content/participant-choice-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChoiceContent } from '../choice-content';
+import { AnswerOption } from '../answer-option';
 
 @Component({
   selector: 'app-participant-choice-content',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participant-choice-content.component.scss']
 })
 export class ParticipantChoiceContentComponent implements OnInit {
-
+  testAnswers = [
+    new AnswerOption('A - Giraffe', '0'),
+    new AnswerOption('B - Bär', '0'),
+    new AnswerOption('C - bra', '10')
+  ];
+  testCorrectAnswers = [2];
+  testChoiceContent = new ChoiceContent(
+    '1', '1', '1', 'Tierkunde', 'Welches Tier ist schwarz mit weißen Streifen?', 1, this.testAnswers, this.testCorrectAnswers, false);
   constructor() { }
 
   ngOnInit() {
