763eb79b1970febb4dea9bc4fe1bad5dff82872e
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-03-16 00:54:23 +0100

    Implement submit function to add or edit choice content Implement function to add Answers to ChoiceContent

--- src/app/creator-choice-content/creator-choice-content.component.ts
+++ src/app/creator-choice-content/creator-choice-content.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AnswerOption } from '../answer-option';
 import { ChoiceContent } from '../choice-content';
+import { ContentService } from '../content.service';
+
+export class CorrectAnswer {
+  answerOption: AnswerOption;
+  correct: boolean;
+
+  constructor(answerOption: AnswerOption, correct: boolean) {
+    this.answerOption = answerOption;
+    this.correct = correct;
+  }
+}
 
 @Component({
   selector: 'app-creator-choice-content',
@@ -9,7 +20,7 @@ import { ChoiceContent } from '../choice-content';
 })
 export class CreatorChoiceContentComponent implements OnInit {
 
-  content: ChoiceContent = new ChoiceContent('2',
+  content: ChoiceContent = new ChoiceContent('0',
     '1',
     '1',
     'Choice Content 1',
@@ -21,20 +32,37 @@ export class CreatorChoiceContentComponent implements OnInit {
       new AnswerOption('Option 3', '20'),
       new AnswerOption('Option 4', '30')
     ],
-    [1, 2, 3],
+    [0, 1, 3],
     true);
 
   displayedColumns = ['label', 'points'];
 
-  constructor() {
+  correctAnswers: CorrectAnswer[] = [];
+
+  constructor(private contentService: ContentService) {
   }
 
   ngOnInit() {
+    for (let i = 0; i < this.content.options.length; i++) {
+      this.correctAnswers.push(new CorrectAnswer(this.content.options[i], this.content.correctOptionIndexes.includes(i)));
+    }
+    console.log(this.correctAnswers);
   }
 
   submitContent() {
+    if (this.content.contentId === '0') {
+      this.contentService.addContent(this.content).subscribe();
+    } else {
+      // ToDo: Implement function in service
+      // this.contentService.updateContent(this.content).subscribe();
+    }
   }
 
-  addAnswer(isCorrect: boolean, label: string, points: number) {
+  addAnswer(isCorrect: boolean, label: string, points: string) {
+    this.content.options.push(new AnswerOption(label, points));
+    if (isCorrect) {
+      this.content.correctOptionIndexes.push(this.content.options.length);
+    }
+    this.submitContent();
   }
 }
