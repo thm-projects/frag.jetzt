f7fce1dd0cb1e4ac802495cc978e4b692b48ebd2
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-15 12:49:27 +0100

    Implement fake-data in component.ts

--- src/app/creator-choice-content/creator-choice-content.component.ts
+++ src/app/creator-choice-content/creator-choice-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnswerOption } from '../answer-option';
 
 @Component({
   selector: 'app-creator-choice-content',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creator-choice-content.component.scss']
 })
 export class CreatorChoiceContentComponent implements OnInit {
+  subject = 'TestSubject';
+  body = 'TestBody';
+  newLabel: string;
+  newPoints: string;
+  options = [
+    new AnswerOption('A - Giraffe', '0'),
+    new AnswerOption('B - Bär', '0'),
+    new AnswerOption('C - bra', '10')
+    ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addAnswer() {
+    this.options.push(new AnswerOption(this.newLabel, this.newPoints));
+    this.newLabel = '';
+    this.newPoints = '';
+  }
 }
