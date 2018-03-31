441fc54a261fc25d01d3fc05c3eeac2a232936b3
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-03-31 11:37:14 +0200

    Add dummy-content to build yes or no - component's view

--- src/app/components/fragments/content-yes-no-creator/content-yes-no-creator.component.ts
+++ src/app/components/fragments/content-yes-no-creator/content-yes-no-creator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentChoice } from '../../../models/content-choice';
+import { DisplayAnswer } from '../content-choice-creator/content-choice-creator.component';
+import { AnswerOption } from '../../../models/answer-option';
 
 @Component({
   selector: 'app-content-yes-no-creator',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-yes-no-creator.component.scss']
 })
 export class ContentYesNoCreatorComponent implements OnInit {
+  answerLabels = [
+    'yes',
+    'no'
+  ];
+  content: ContentChoice = new ContentChoice('0',
+    '1',
+    '',
+    '',
+    '',
+    1,
+    [],
+    [],
+    false);
 
-  constructor() { }
+  displayedColumns = ['label', 'points'];
 
-  ngOnInit() {
+  displayAnswers: DisplayAnswer[] = [];
+  newAnswerOptionPoints = '';
+
+  constructor() {
   }
 
+  ngOnInit() {
+    for (let i = 0; i < this.answerLabels.length; i++) {
+      this.content.options.push(new AnswerOption(this.answerLabels[i], this.newAnswerOptionPoints));
+    }
+    this.fillCorrectAnswers();
+  }
+
+  fillCorrectAnswers() {
+    this.displayAnswers = [];
+    for (let i = 0; i < this.content.options.length; i++) {
+      this.displayAnswers.push(new DisplayAnswer(this.content.options[i], this.content.correctOptionIndexes.includes(i)));
+    }
+  }
+
+  submitContent(): void {
+    console.log('submit content');
+  }
 }
