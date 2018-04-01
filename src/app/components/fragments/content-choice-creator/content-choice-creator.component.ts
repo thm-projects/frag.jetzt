16ae3dc403f92689c31a9c7056c0a51f8ff79059
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-04-01 11:52:57 +0200

    Add logic for choice-creator-component to check for empty answers and to check if there is more then 1 true answer in single-choice-mode

--- src/app/components/fragments/content-choice-creator/content-choice-creator.component.ts
+++ src/app/components/fragments/content-choice-creator/content-choice-creator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AnswerOption } from '../../../models/answer-option';
 import { ContentChoice } from '../../../models/content-choice';
 import { ContentService } from '../../../services/http/content.service';
+import { NotificationService } from '../../../services/util/notification.service';
 
 export class DisplayAnswer {
   answerOption: AnswerOption;
@@ -39,7 +40,8 @@ export class ContentChoiceCreatorComponent implements OnInit {
   newAnswerOptionLabel = '';
   newAnswerOptionPoints = '';
 
-  constructor(private contentService: ContentService) {
+  constructor(private contentService: ContentService,
+              private notificationService: NotificationService) {
   }
 
   ngOnInit() {
@@ -63,6 +65,20 @@ export class ContentChoiceCreatorComponent implements OnInit {
   }
 
   addAnswer() {
+    if (this.newAnswerOptionLabel === '') {
+      this.notificationService.show('No empty answers allowed.');
+      this.newAnswerOptionChecked = false;
+      this.newAnswerOptionLabel = '';
+      this.newAnswerOptionPoints = '';
+      return;
+    }
+    if (this.singleChoice && this.content.correctOptionIndexes.length > 0 && this.newAnswerOptionChecked) {
+      this.notificationService.show('In single choice mode is only 1 true answer allowed.');
+      this.newAnswerOptionChecked = false;
+      this.newAnswerOptionLabel = '';
+      this.newAnswerOptionPoints = '';
+      return;
+    }
     this.content.options.push(new AnswerOption(this.newAnswerOptionLabel, this.newAnswerOptionPoints));
     if (this.newAnswerOptionChecked) {
       this.content.correctOptionIndexes.push(this.content.options.length - 1);
