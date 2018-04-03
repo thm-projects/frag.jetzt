16766f693fa19e6621fd36bdd8a23b01ce9cd50d
Thomas Lenz <Thomas.Lenz@mni.thm.de>
2018-04-03 19:25:33 +0200

    Implement logic for yes-no component to change selection and to check for allowed answer

--- src/app/components/fragments/content-yes-no-creator/content-yes-no-creator.component.ts
+++ src/app/components/fragments/content-yes-no-creator/content-yes-no-creator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ContentChoice } from '../../../models/content-choice';
 import { DisplayAnswer } from '../content-choice-creator/content-choice-creator.component';
 import { AnswerOption } from '../../../models/answer-option';
+import { NotificationService } from '../../../services/util/notification.service';
 
 @Component({
   selector: 'app-content-yes-no-creator',
@@ -28,7 +29,7 @@ export class ContentYesNoCreatorComponent implements OnInit {
   displayAnswers: DisplayAnswer[] = [];
   newAnswerOptionPoints = '';
 
-  constructor() {
+  constructor(private notificationService: NotificationService) {
   }
 
   ngOnInit() {
@@ -44,8 +45,24 @@ export class ContentYesNoCreatorComponent implements OnInit {
       this.displayAnswers.push(new DisplayAnswer(this.content.options[i], this.content.correctOptionIndexes.includes(i)));
     }
   }
+  setCorrect(label: string) {
+    if (label === 'yes') {
+      this.content.correctOptionIndexes = [0];
+    }
+    if (label === 'no') {
+      this.content.correctOptionIndexes = [1];
+    }
+    this.fillCorrectAnswers();
+  }
+  checkAllowedContent(): boolean {
+    return (this.content.correctOptionIndexes.length === 1);
+  }
 
   submitContent(): void {
+    if (!this.checkAllowedContent()) {
+      this.notificationService.show('Select 1 true answer.');
+      return;
+    }
     console.log('submit content');
   }
 }
