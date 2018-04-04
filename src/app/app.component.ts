9f73b53fa33f1f60aab9fea7f46f11e2a013b514
Lukas Kimpel <lukas.kimpel@mni.thm.de>
2018-04-04 15:28:54 +0200

    Init translate module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ARSnova';
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+  }
+
 }
