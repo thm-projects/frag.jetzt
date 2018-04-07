56d9122edbf0d748a247e392c05ec83d7dbb65fc
Hagen <hagen.dressler@mni.thm.de>
2018-04-07 12:50:36 +0200

    Edit html component feedback-barometer (add bars, mat-card)

--- src/app/components/pages/feedback-barometer-page/feedback-barometer-page.component.ts
+++ src/app/components/pages/feedback-barometer-page/feedback-barometer-page.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback-barometer-page.component.scss']
 })
 export class FeedbackBarometerPageComponent implements OnInit {
+  feedback: any = [
+    { state: 'good', count: 0, },
+    { state: 'ok', count: 0, },
+    { state: 'not ok', count: 0, },
+    { state: 'bad', count: 0, },
+  ];
 
   constructor() { }
 
