5f7a3d72227520d433d9280ca6bff73c645b933b
Hagen <hagen.dressler@mni.thm.de>
2018-04-07 14:28:25 +0200

    Add logic to feedback-barometer (add click and update events)

--- src/app/components/pages/feedback-barometer-page/feedback-barometer-page.component.ts
+++ src/app/components/pages/feedback-barometer-page/feedback-barometer-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../services/http/authentication.service';
+import { UserRole } from '../../../models/user-roles.enum';
 
 @Component({
   selector: 'app-feedback-barometer-page',
@@ -7,15 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackBarometerPageComponent implements OnInit {
   feedback: any = [
-    { state: 'good', count: 0, },
-    { state: 'ok', count: 0, },
-    { state: 'not ok', count: 0, },
-    { state: 'bad', count: 0, },
+    { state: 0, name: 'good', count: 0, },
+    { state: 1, name: 'ok', count: 0, },
+    { state: 2, name: 'not-ok', count: 0, },
+    { state: 3, name: 'bad', count: 0, },
   ];
+  userRole: UserRole;
 
-  constructor() { }
+  dummy = [2, 3, 0, 1]; // dummy data -> delete this with api implementation and add get-data
+
+  constructor(private authenticationService: AuthenticationService) {}
 
   ngOnInit() {
+    this.userRole = this.authenticationService.getRole();
+    this.updateFeedback(this.dummy);
   }
 
+  private updateFeedback(data) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const sum = data.reduce(reducer);
+    for (let i = 0; i < this.feedback.length; i++) {
+      this.feedback[i].count = data[i] / sum * 100;
+    }
+  }
+
+  submitFeedback(state: string) {
+    this.dummy[state] += 1; // delete this with api implementation and add submit-data
+    this.updateFeedback(this.dummy);
+  }
+
+  toggle() {
+    // api feature is yet not implemented
+  }
 }
