0ff925a40516693908aff0e6a89563f13ddaca56
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-11-07 15:09:16 +0100

    Refactor likert-content-creation

--- src/app/components/creator/content-likert-creator/content-likert-creator.component.ts
+++ src/app/components/creator/content-likert-creator/content-likert-creator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { DisplayAnswer } from '../content-choice-creator/content-choice-creator.component';
 import { ContentChoice } from '../../../models/content-choice';
 import { AnswerOption } from '../../../models/answer-option';
@@ -19,6 +19,10 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./content-likert-creator.component.scss']
 })
 export class ContentLikertCreatorComponent implements OnInit {
+  @Input() contentSub;
+  @Input() contentBod;
+  @Input() contentCol;
+
   likertScale = [
     'Strongly agree',
     'Agree',
@@ -98,8 +102,8 @@ export class ContentLikertCreatorComponent implements OnInit {
     });
   }
 
-  submitContent(subject: string, body: string, group: string): void {
-    if (subject.valueOf() === '' || body.valueOf() === '') {
+  submitContent(): void {
+    if (this.contentSub === '' || this.contentBod === '') {
       this.translationService.get('content.no-empty').subscribe(message => {
         this.notificationService.show(message);
       });
@@ -109,10 +113,10 @@ export class ContentLikertCreatorComponent implements OnInit {
       '',
       '',
       this.roomId,
-      subject,
-      body,
+      this.contentSub,
+      this.contentBod,
       1,
-      [group],
+      [this.contentCol],
       this.content.options,
       this.content.correctOptionIndexes,
       this.content.multiple,
