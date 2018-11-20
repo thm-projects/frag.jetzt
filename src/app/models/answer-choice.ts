9ab66e02a18f348e5aedd9d4be5b50e5e377aab3
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-11-20 15:51:21 +0100

    Fix content-choice answer sending

--- src/app/models/answer-choice.ts
+++ src/app/models/answer-choice.ts
@@ -1,7 +1,11 @@
+import { ContentType } from './content-type.enum';
+
 export class AnswerChoice {
   id: string;
   revision: string;
   contentId: string;
   round: number;
   selectedChoiceIndexes: number[];
+  creationTimestamp: Date;
+  format: ContentType;
 }
