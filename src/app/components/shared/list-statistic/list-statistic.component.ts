c792a85c10f114d030ec26e76850c4d90293d1d8
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-11-28 23:40:21 +0100

    Implement api connection for answer-statistic and handle data

--- src/app/components/shared/list-statistic/list-statistic.component.ts
+++ src/app/components/shared/list-statistic/list-statistic.component.ts
@@ -2,12 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ContentGroup } from '../../../models/content-group';
 import { ContentService } from '../../../services/http/content.service';
 import { Content } from '../../../models/content';
+import { ContentType } from '../../../models/content-type.enum';
+import { AnswerOption } from '../../../models/answer-option';
+import { ContentChoice } from '../../../models/content-choice';
 
 export class ContentPercents {
-  content: string;
+  content: Content;
   percent: string;
 
-  constructor(content: string, percent: string) {
+  constructor(content: Content, percent: string) {
     this.content = content;
     this.percent = percent;
   }
@@ -26,26 +29,59 @@ export class ListStatisticComponent implements OnInit {
   percents: number[] = [];
   displayedColumns = ['content', 'percentage'];
   dataSource: ContentPercents[];
+  total = 0;
+  totalP = 0;
+  correctCounts = 0;
+  totalCounts = 0;
 
   constructor(private contentService: ContentService) {
   }
 
   ngOnInit() {
     this.percents = [73, 87, 69, 92, 77];
-    this.contentService.getContentsByIds(this.contentGroup.contentIds).subscribe(contents => {
+    this.contentService.getContentChoiceByIds(this.contentGroup.contentIds).subscribe(contents => {
       this.getContents(contents);
     });
   }
 
-  getContents(contents: Content[]) {
+  getContents(contents: ContentChoice[]) {
      this.contents = contents;
      const length = contents.length;
      this.dataSource = new Array<ContentPercents>(length);
      for (let i = 0; i < length; i++) {
-      this.dataSource[i] = new ContentPercents('', '' );
-      this.dataSource[i].content = this.contents[i].subject;
-      this.dataSource[i].percent = this.percents[i].toFixed(0);
-    }
+       this.dataSource[i] = new ContentPercents(null, '' );
+       this.dataSource[i].content = this.contents[i];
+       if (contents[i].format === ContentType.CHOICE) {
+         this.contentService.getAnswer(contents[i].id).subscribe(answer => {
+           let percent = this.getCountCorrect(contents[i].options, answer.roundStatistics[0].independentCounts);
+           this.dataSource[i].percent = percent.toFixed() + ' %';
+           this.totalP += percent;
+           this.total = this.totalP / length;
+         });
+       } else {
+         this.dataSource[i].percent = 'NO';
+       }
+     }
   }
 
+  getCountCorrect(options: AnswerOption[], indCounts: number[]): number {
+    let correctIndex;
+    this.correctCounts = 0;
+    this.totalCounts = 0;
+    const length = options.length;
+    let res: number;
+    for (let i = 0; i < length; i++) {
+      if (options[i].points > 0) {
+        correctIndex = i;
+      }
+    }
+    for (let i = 0; i < length; i++) {
+      if (i === correctIndex) {
+        this.correctCounts += indCounts[i];
+      }
+      this.totalCounts += indCounts[i];
+    }
+    res = ((this.correctCounts / this.totalCounts) * 100);
+    return res;
+  }
 }
