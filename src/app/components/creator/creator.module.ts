e7db12e2df18e3aa7937939c86d1e7dfd5621f8f
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-11-28 12:45:57 +0100

    Add new component 'list-statistic'

--- src/app/components/creator/creator.module.ts
+++ src/app/components/creator/creator.module.ts
@@ -22,6 +22,7 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ContentCreatorComponent } from './content-creator/content-creator.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { ListStatisticComponent } from './list-statistic/list-statistic.component';
 
 @NgModule({
   imports: [
@@ -52,7 +53,8 @@ import { ContentListComponent } from './content-list/content-list.component';
     RoomDeleteComponent,
     RoomEditComponent,
     ContentCreatorComponent,
-    ContentListComponent
+    ContentListComponent,
+    ListStatisticComponent
   ],
   entryComponents: [
     RoomCreateComponent,
---
+++ src/app/components/creator/list-statistic/list-statistic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-statistic',
+  templateUrl: './list-statistic.component.html',
+  styleUrls: ['./list-statistic.component.scss']
+})
+export class ListStatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
