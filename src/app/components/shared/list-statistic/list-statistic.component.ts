6b2d1a02f3078c95afdfae276b8f4a953efc9761
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-11-28 17:42:25 +0100

    Fix dataSource and several bugs

--- src/app/components/shared/list-statistic/list-statistic.component.ts
+++ src/app/components/shared/list-statistic/list-statistic.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ContentGroup } from '../../../models/content-group';
+import { ContentService } from '../../../services/http/content.service';
+import { Content } from '../../../models/content';
+
+export class ContentPercents {
+  content: string;
+  percent: string;
+
+  constructor(content: string, percent: string) {
+    this.content = content;
+    this.percent = percent;
+  }
+}
 
 @Component({
   selector: 'app-list-statistic',
@@ -7,9 +20,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListStatisticComponent implements OnInit {
 
-  constructor() { }
+  @Input() contentGroup: ContentGroup;
+
+  contents: Content[] = [];
+  percents: number[] = [];
+  displayedColumns = ['content', 'percentage'];
+  dataSource: ContentPercents[];
+
+  constructor(private contentService: ContentService) {
+  }
 
   ngOnInit() {
+    this.percents = [73, 87, 69, 92, 77];
+    this.contentService.getContentsByIds(this.contentGroup.contentIds).subscribe(contents => {
+      this.getContents(contents);
+    });
+  }
+
+  getContents(contents: Content[]) {
+     this.contents = contents;
+     const length = contents.length;
+     this.dataSource = new Array<ContentPercents>(length);
+     for (let i = 0; i < length; i++) {
+      this.dataSource[i] = new ContentPercents('', '' );
+      this.dataSource[i].content = this.contents[i].subject;
+      this.dataSource[i].percent = this.percents[i].toFixed(0);
+    }
   }
 
 }
