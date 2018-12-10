fff745eeec2cbd7c1361d129de0dcf99ccf9928a
Lukas Mauß <lukas.mauss@mni.thm.de>
2018-12-10 14:37:02 +0100

    Add some fake data for testing

--- src/app/components/shared/statistic/statistic.component.ts
+++ src/app/components/shared/statistic/statistic.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticComponent implements OnInit {
 
+  chartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  chartLabels = ['a', 'b', 'c', 'd'];
+  chartType = 'bar';
+  chartLegend = true;
+
+  chartData = [
+    { data: [12, 30, 43, 32], label: 'dataOne' },
+    { data: [8, 20, 33, 12], label: 'dataTwo' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
