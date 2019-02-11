5fe52a347f152bcf8d9c975100616bff209e159c
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-11 15:15:07 +0100

    Start implementing form tests

--- src/app/components/creator/_dialogs/content-edit/content-edit.component.ts
+++ src/app/components/creator/_dialogs/content-edit/content-edit.component.ts
@@ -4,6 +4,8 @@ import { ContentListComponent } from '../../content-list/content-list.component'
 import { DisplayAnswer } from '../../content-choice-creator/content-choice-creator.component';
 import { ContentChoice } from '../../../../models/content-choice';
 import { AnswerOption } from '../../../../models/answer-option';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationService } from '../../../../services/util/notification.service';
 
 @Component({
   selector: 'app-content-edit',
@@ -15,7 +17,9 @@ export class ContentEditComponent implements OnInit {
   displayAnswers: DisplayAnswer[] = [];
   displayedColumns = ['label', 'checked'];
 
-  constructor(public dialogRef: MatDialogRef<ContentListComponent>,
+  constructor(private translateService: TranslateService,
+              private notificationService: NotificationService,
+              public dialogRef: MatDialogRef<ContentListComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
@@ -27,11 +31,42 @@ export class ContentEditComponent implements OnInit {
     }
   }
 
-  update(index: number) {
+  updateAnswer(index: number) {
     if (this.displayAnswers[index].correct === true) {
       this.content.options[index].points = 10;
     } else {
       this.content.options[index].points = -10;
+    }
+  }
+
+  updateContent() {
+    if (this.content.subject === '' || this.content.body === '') {
+      this.translateService.get('content.no-empty').subscribe(message => {
+        this.notificationService.show(message);
+      });
+      return;
+    }
+    if (this.content.options.length === 0) {
+      this.translateService.get('content.need-answers').subscribe(message => {
+        this.notificationService.show(message);
+      });
+      return;
+    }
+    for (let i = 0; i < this.content.options.length; i++) {
+      if (this.content.options[i].label === '') {
+        this.translateService.get('content.no-empty2').subscribe(message => {
+          this.notificationService.show(message);
+        });
+        return;
+      }
+      if (this.content.options[i].points > 0 && this.content.multiple) {
+        this.dialogRef.close('update');
+      } else {
+        this.translateService.get('content.at-least-one').subscribe(message => {
+          this.notificationService.show(message);
+        });
+        return;
+      }
     }
   }
 }
