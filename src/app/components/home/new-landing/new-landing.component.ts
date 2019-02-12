4a9566651be998f4918103b421d3c623f498356f
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-12 22:09:26 +0100

    Add login function to room-joining

--- src/app/components/home/new-landing/new-landing.component.ts
+++ src/app/components/home/new-landing/new-landing.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomCreateComponent } from '../../creator/_dialogs/room-create/room-create.component';
 import { MatDialog } from '@angular/material';
-import { RoomService } from '../../../services/http/room.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Room } from '../../../models/room';
+import { AuthenticationService } from '../../../services/http/authentication.service';
+import { User } from '../../../models/user';
+import { UserRole } from '../../../models/user-roles.enum';
 
 export class Session {
   name: string;
@@ -23,19 +23,28 @@ export class Session {
 export class NewLandingComponent implements OnInit {
 
   sessions: Session[] = new Array<Session>();
+  user: User;
 
-  constructor(public dialog: MatDialog) {
+  constructor(public authenticationService: AuthenticationService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.sessions[0] = new Session('Angular', 98299243);
     this.sessions[1] = new Session('Typescript', 52009627);
     this.sessions[2] = new Session('Angular', 48590407);
+    this.authenticationService.watchUser.subscribe(newUser => this.user = newUser);
   }
 
   openCreateRoomDialog(): void {
     this.dialog.open(RoomCreateComponent, {
       width: '350px'
     });
+  }
+
+  login(): void {
+    if (!this.user) {
+      this.authenticationService.guestLogin(UserRole.PARTICIPANT).subscribe();
+    }
   }
 }
