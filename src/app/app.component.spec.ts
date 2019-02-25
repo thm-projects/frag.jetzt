82ea334a2d536d626a42d218275e7d82fb44bef3
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-25 22:10:28 +0100

    Remove useless test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,10 +33,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ARSnova');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
 });
