6f18427692997e7ddf338a8aa73aac416b1e2a54
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-25 20:45:26 +0100

    Fix first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,16 +3,24 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './components/shared/shared.module';
 import { RouterTestingModule } from '@angular/router/testing'
 import { AuthenticationService } from './services/http/authentication.service';
+import { DataStoreService } from './services/util/data-store.service';
+import { NotificationService } from './services/util/notification.service';
+import { LanguageService } from './services/util/language.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ SharedModule,
-                 RouterTestingModule,
-                 AuthenticationService ],
+                 RouterTestingModule ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        AuthenticationService,
+        DataStoreService,
+        NotificationService,
+        LanguageService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
