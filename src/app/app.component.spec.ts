0a45ca6df057576760b2b0f8ab29816a73851628
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-25 22:08:35 +0100

    Fix title test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,10 +28,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'ARSnova'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('ARSnova');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
