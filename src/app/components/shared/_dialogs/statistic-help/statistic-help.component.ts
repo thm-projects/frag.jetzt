419f569a7a3ca685bf2796610053153588773122
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-02-26 02:46:10 +0100

    Create new component 'statistic-help'

---
+++ src/app/components/shared/_dialogs/statistic-help/statistic-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistic-help',
+  templateUrl: './statistic-help.component.html',
+  styleUrls: ['./statistic-help.component.scss']
+})
+export class StatisticHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
