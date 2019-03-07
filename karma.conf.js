e05b3b715dbb8aec739cd48d21206ec2387b67b7
Tom Käsler <tom.kaesler@mni.thm.de>
2019-03-07 15:28:33 +0100

    add custom chrome with --no-sandbox flag to be docker compatible

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    browsers : ['ChromeHeadlessCustom'],
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
