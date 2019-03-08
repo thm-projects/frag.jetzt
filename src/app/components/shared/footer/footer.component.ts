e9835e2057680b48573fd62bb4184b9c0eee9a47
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-03-08 17:37:32 +0100

    Add i18n to footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,7 +1,9 @@
+import { LanguageService } from './../../../services/util/language.service';
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../../../services/util/notification.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -13,18 +15,29 @@ export class FooterComponent implements OnInit {
   blogUrl = 'https://arsnova.thm.de/blog/';
   dsgvoUrl = 'https://arsnova.thm.de/blog/datenschutzerklaerung/';
   imprUrl = 'https://arsnova.thm.de/blog/impressum/';
-  constructor(
-    public notificationService: NotificationService,
-    public router: Router,
-    public dialog: MatDialog
-  ) { }
+
+  open: string;
+
+  constructor(public notificationService: NotificationService,
+              public router: Router,
+              public dialog: MatDialog,
+              private translateService: TranslateService,
+              private langService: LanguageService) {
+                langService.langEmitter.subscribe(lang => translateService.use(lang));
+              }
 
   ngOnInit() {
+    this.translateService.use(localStorage.getItem('currentLang'));
+    this.translateService.get('footer.open').subscribe(message => {
+      this.open = message;
+    });
   }
 
   navToBlog() {
-    this.notificationService.show('Der Blog wird in einem neuen Fenster geöffnet..', 'Öffnen' , {
-      duration: 4000
+    this.translateService.get('footer.will-open').subscribe(message => {
+      this.notificationService.show('Blog' + message, this.open, {
+        duration: 4000
+      });
     });
     this.notificationService.snackRef.afterDismissed().subscribe(info => {
       if (info.dismissedByAction === true) {
@@ -33,8 +46,12 @@ export class FooterComponent implements OnInit {
     });
   }
   navToDSGVO() {
-    this.notificationService.show('Die Datenschutzverordnung wird in einem neuen Fenster geöffnet..', 'Öffnen' , {
-      duration: 5000
+    this.translateService.get('footer.will-open').subscribe(message => {
+      this.translateService.get('footer.dsgvo').subscribe(what => {
+        this.notificationService.show(what + message, this.open, {
+          duration: 4000
+        });
+      });
     });
     this.notificationService.snackRef.afterDismissed().subscribe(info => {
       if (info.dismissedByAction === true) {
@@ -43,8 +60,12 @@ export class FooterComponent implements OnInit {
     });
   }
   navToImprint() {
-    this.notificationService.show('Das Impressum wird in einem neuen Fenster geöffnet..', 'Öffnen' , {
-      duration: 5000
+    this.translateService.get('footer.will-open').subscribe(message => {
+      this.translateService.get('footer.imprint').subscribe(what => {
+        this.notificationService.show(what + message, this.open, {
+          duration: 4000
+        });
+      });
     });
     this.notificationService.snackRef.afterDismissed().subscribe(info => {
       if (info.dismissedByAction === true) {
