57f254532d027586247718a04cb528fc07d97111
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-03-08 18:37:59 +0100

    Create presentation component

---
+++ src/app/components/creator/content-presentation/content-presentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-presentation',
+  templateUrl: './content-presentation.component.html',
+  styleUrls: ['./content-presentation.component.scss']
+})
+export class ContentPresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
