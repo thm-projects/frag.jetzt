c57afa432b91450d8d6afbd2574feef75bf65825
Louis Peter <Louis.Peter@mni.thm.de>
2019-03-18 10:54:40 +0100

    Create new comment component

---
+++ src/app/components/shared/comment/comment.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss']
+})
+export class CommentComponent implements OnInit {
+  @Input() comment;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
