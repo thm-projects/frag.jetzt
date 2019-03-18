3d6d9b95b2e4b36df07cc238e28c00c26841b70f
Anris Ceta <anris.ceta@mni.thm.de>
2019-03-18 15:41:06 +0100

    fix style

--- src/app/components/shared/comment/comment.component.ts
+++ src/app/components/shared/comment/comment.component.ts
@@ -37,6 +37,7 @@ export class CommentComponent implements OnInit {
   }
 
   setRead(comment: Comment): void {
+    comment.read = !comment.read;
     this.commentService.updateComment(comment).subscribe();
   }
 
