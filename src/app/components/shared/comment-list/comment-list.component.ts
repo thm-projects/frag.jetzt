5d22cc7120cc7a8f719c45b4fa285b30d7346bc7
Louis Peter <Louis.Peter@mni.thm.de>
2019-03-18 10:56:39 +0100

    Change comment list to load comment components

--- src/app/components/shared/comment-list/comment-list.component.ts
+++ src/app/components/shared/comment-list/comment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Comment } from '../../../models/comment';
