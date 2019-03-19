b27202c56c386f672070a0c07a969f136b79c725
Thisari Muthuwahandi <thisari.muthuwahandi@mni.thm.de>
2019-03-19 12:52:47 +0100

    design the search function

--- src/app/components/shared/comment-list/comment-list.component.ts
+++ src/app/components/shared/comment-list/comment-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Comment } from '../../../models/comment';
 import { CommentService } from '../../../services/http/comment.service';
 import { TranslateService } from '@ngx-translate/core';
@@ -15,32 +14,35 @@ export class CommentListComponent implements OnInit {
   comments: Comment[];
   isLoading = true;
   roomId: string;
-  private searchTerms = new Subject<string>();
-  comments$: Observable<Comment[]>;
+  private hideCommentsList: boolean;
+  private comments$: Observable<Comment[]>;
 
   constructor(private commentService: CommentService,
-              private translateService: TranslateService,
-              protected langService: LanguageService) {
+    private route: ActivatedRoute,
+    private roomService: RoomService,
+    private location: Location,
+    private commentService: CommentService,
+    private notification: NotificationService,
+    private translateService: TranslateService,
+    protected langService: LanguageService) {
     langService.langEmitter.subscribe(lang => translateService.use(lang));
   }
 
   ngOnInit() {
     this.roomId = localStorage.getItem(`roomId`);
+    this.hideCommentsList = false;
     this.getComments();
-     this.comments$ = this.searchTerms.pipe(
-      debounceTime(100),
-      distinctUntilChanged(),
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.commentService.searchComments(this.roomId, term)),
-    ); 
-    this.translateService.use(localStorage.getItem('currentLang')); 
+    this.translateService.use(localStorage.getItem('currentLang'));
   }
 
   getComments(): void {
-      this.commentService.getComments(this.roomId)
-        .subscribe(comments => {
-          this.comments = comments;
-          this.isLoading = false;
-        });
+    this.commentService.getComments(this.roomId)
+      .subscribe(comments => {
+        this.comments = comments;
+        this.isLoading = false;
+      });
+  }
+
+  search(term: string): void {
   }
 }
