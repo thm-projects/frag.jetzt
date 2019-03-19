8a8a3480af20b7086d291a0afd39cb1295a89716
Anris Ceta <anris.ceta@mni.thm.de>
2019-03-19 12:41:03 +0100

    Outsource comment-create-component

---
+++ src/app/components/shared/comments-creator-shared/comments-creator-shared.component.spec.ts
@@ -0,0 +1,25 @@
+/* import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentsCreatorSharedComponent } from './comments-creator-shared.component';
+
+describe('CommentsCreatorSharedComponent', () => {
+  let component: CommentsCreatorSharedComponent;
+  let fixture: ComponentFixture<CommentsCreatorSharedComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CommentsCreatorSharedComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentsCreatorSharedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+}); */
---
+++ src/app/components/shared/comments-creator-shared/comments-creator-shared.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { Comment } from '../../../models/comment';
+import { CommentService } from '../../../services/http/comment.service';
+import { NotificationService } from '../../../services/util/notification.service';
+import { AuthenticationService } from '../../../services/http/authentication.service';
+import { User } from '../../../models/user';
+import { CommentListComponent } from '../comment-list/comment-list.component';
+
+@Component({
+  selector: 'app-comments-creator-shared',
+  templateUrl: './comments-creator-shared.component.html',
+  styleUrls: ['./comments-creator-shared.component.scss']
+})
+export class CommentsCreatorSharedComponent implements OnInit {
+
+  @ViewChild(CommentListComponent) child: CommentListComponent;
+  roomId: string;
+  roomShortId: string;
+  user: User;
+  private date = new Date(Date.now());
+  subjectForm = new FormControl('', [Validators.required]);
+  bodyForm = new FormControl('', [Validators.required]);
+
+
+  constructor(
+    protected authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private commentService: CommentService,
+    private notification: NotificationService,
+    private translationService: TranslateService) { }
+
+  ngOnInit(): void {
+    this.user = this.authenticationService.getUser();
+    this.roomShortId = this.route.snapshot.paramMap.get('roomId');
+    this.roomId = localStorage.getItem(`roomId`);
+  }
+
+  send(subject: string, body: string): void {
+    subject = subject.trim();
+    body = body.trim();
+    if (!subject && !body) {
+      this.translationService.get('comment-page.error-both-fields').subscribe(message => {
+        this.notification.show(message);
+      });
+      return;
+    }
+    if (!subject) {
+      this.translationService.get('comment-page.error-title').subscribe(message => {
+        this.notification.show(message);
+      });
+      return;
+    }
+    if (!body) {
+      this.translationService.get('comment-page.error-comment').subscribe(message => {
+        this.notification.show(message);
+      });
+      return;
+    }
+    this.commentService.addComment({
+      id: '',
+      roomId: this.roomId,
+      userId: this.user.id,
+      subject: subject,
+      body: body,
+      creationTimestamp: this.date.getTime(),
+      read: false,
+      revision: ''
+    } as Comment).subscribe(() => {
+      this.child.getComments();
+      this.notification.show(`Comment '${subject}' successfully created.`);
+    });
+  }
+}
