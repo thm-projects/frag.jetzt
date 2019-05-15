a2d9350e577783ce951f9210386060581cc803ba
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-05-15 22:21:22 +0200

    Fix export

--- src/app/components/shared/comment-list/comment-list.component.ts
+++ src/app/components/shared/comment-list/comment-list.component.ts
@@ -170,11 +170,46 @@ export class CommentListComponent implements OnInit {
     this.wsCommentService.add(comment);
   }
 
+  exportCsv(delimiter: string, date: string): void {
+    let exportComments = JSON.parse(JSON.stringify(this.comments));
+    let csv: string;
+    let keyFields = '';
+    let valueFields = '';
+    keyFields = Object.keys(exportComments[0]).slice(3).join(delimiter) + '\r\n';
+    exportComments.forEach(element => {
+      element.body = '"' + element.body.replace(/[\r\n]/g, ' ').replace(/ +/g, ' ').replace(/"/g, '""') + '"';
+      valueFields += Object.values(element).slice(3).join(delimiter) + '\r\n';
+    });
+    csv = keyFields + valueFields;
+    const myBlob = new Blob([csv], { type: 'text/csv' });
+    const link = document.createElement('a');
+    const fileName = 'comments_' + date + '.csv';
+    link.setAttribute('download', fileName);
+    link.href = window.URL.createObjectURL(myBlob);
+    link.click();
+  }
+
+  onExport(exportType: string): void {
+    const date = new Date();
+    const dateString = date.getFullYear() + '_' + ('0' + (date.getMonth() + 1)).slice(-2) + '_' + ('0' + date.getDate()).slice(-2);
+    const timeString = ('0' + date.getHours()).slice(-2) + ('0' + date.getMinutes()).slice(-2) + ('0' + date.getSeconds()).slice(-2);
+    const timestamp = dateString + '_' + timeString;
+      if (exportType === 'comma') {
+        this.exportCsv(',', timestamp);
+      }
+      if (exportType === 'semicolon') {
+        this.exportCsv(';', timestamp);
+      }
+  }
+
+
   openExportDialog(): void {
     const dialogRef = this.dialog.open(CommentExportComponent, {
       width: '400px'
     });
-    dialogRef.componentInstance.comments = this.comments;
+    dialogRef.afterClosed().subscribe(result => {
+      this.onExport(result);
+    });
   }
 
   filterFavorite(): void {
