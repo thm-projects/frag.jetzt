2b03fe880c83d3d467e21b622bc23fdaae4b3b41
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-05-25 21:48:19 +0200

    Add purple-theme

--- src/theme/arsnova-theme.const.ts
+++ src/theme/arsnova-theme.const.ts
@@ -1,9 +1,11 @@
 import { dark } from './dark-theme/darkTheme.const';
 import { arsnova } from './default-theme/defaultTheme.const';
 import { blue } from './blue-theme/blueTheme.const';
+import { purple } from './purple-theme/purpleTheme.const';
 
 export const themes = {
   arsnova: arsnova,
   dark: dark,
-  blue: blue
+  blue: blue,
+  purple: purple
 };
---
+++ src/theme/purple-theme/purpleTheme.const.ts
@@ -0,0 +1,27 @@
+export const purple = {
+
+  '--primary' : '#9c27b0',
+  '--primary-variant': '#d05ce3',
+
+  '--secondary': '#9c27b0',
+  '--secondary-variant': '#d05ce3',
+
+  '--background': '#f5f5f5',
+  '--surface': '#bdbdbd',
+  '--dialog': '#c2c2c2',
+
+  '--on-primary': '#FFFFFF',
+  '--on-secondary': '#FFFFFF',
+  '--on-background': '#212121',
+  '--on-surface': '#212121',
+
+  '--green': '#AED581',
+  '--red': '#FF8A80',
+  '--yellow': '#FFD54F',
+  '--blue': '#3f51b5',
+  '--purple': '#9c27b0',
+  '--teal': '#4DB6AC',
+  '--grey': '#BDBDBD',
+  '--grey-light': '#EEEEEE',
+  '--black': '#212121'
+};
