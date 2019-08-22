c911d355e76d399bd6dfded5fe63bc63f421236f
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-08-22 17:06:37 +0200

    Add associated rooms of user to confirm dialog

--- src/app/components/shared/_dialogs/delete-account/delete-account.component.ts
+++ src/app/components/shared/_dialogs/delete-account/delete-account.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { RoomEditComponent } from '../../../creator/_dialogs/room-edit/room-edit.component';
+import { RoomService } from '../../../../services/http/room.service';
+import { Room } from '../../../../models/room';
 
 @Component({
   selector: 'app-delete-account',
@@ -9,10 +11,16 @@ import { RoomEditComponent } from '../../../creator/_dialogs/room-edit/room-edit
 })
 export class DeleteAccountComponent implements OnInit {
 
+  rooms: Room[];
+
   constructor(public dialogRef: MatDialogRef<RoomEditComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) { }
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private roomService: RoomService) { }
 
   ngOnInit() {
+    this.roomService.getCreatorRooms().subscribe(rooms => {
+      this.rooms = rooms;
+    });
   }
 
   close(type: string): void {
