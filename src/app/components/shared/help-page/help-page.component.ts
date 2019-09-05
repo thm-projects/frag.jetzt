3fb6c74890aaac1b8dcd7d5aeaf2e84ba1c2f52e
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-05 12:13:01 +0200

    Add language check to display the appropriate text

--- src/app/components/shared/help-page/help-page.component.ts
+++ src/app/components/shared/help-page/help-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/util/language.service';
 
 @Component({
   selector: 'app-help',
@@ -9,9 +11,40 @@ import { Location } from '@angular/common';
 export class HelpPageComponent implements OnInit {
 
   deviceType: string;
-  constructor(private location: Location) { }
+  helptext: string;
+
+  constructor(private location: Location,
+              private translationService: TranslateService,
+              private languageService: LanguageService) {
+  }
 
   ngOnInit() {
+    if (!localStorage.getItem('currentLang')) {
+      const lang = this.translationService.getBrowserLang();
+      this.translationService.setDefaultLang(lang);
+      localStorage.setItem('currentLang', lang);
+    } else {
+      this.translationService.setDefaultLang(localStorage.getItem('currentLang'));
+    }
+    this.getHelptextByLanguage();
+    this.checkForLanguageChange();
+  }
+
+  getHelptextByLanguage() {
+    if (localStorage.getItem('currentLang') === 'de') {
+      this.helptext = 'Das ist deutsch';
+      console.log('0');
+    } else if (localStorage.getItem('currentLang') === 'en') {
+      this.helptext = 'This is english';
+      console.log('1');
+    } else {
+      this.helptext = 'No such language found.';
+      console.log('-1');
+    }
+  }
+
+  checkForLanguageChange() {
+    this.languageService.langEmitter.subscribe(() => this.getHelptextByLanguage());
   }
 
   goBack() {
