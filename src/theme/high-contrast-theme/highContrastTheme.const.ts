9c69771a35cab59764bcf3b332011c3ba49a2a88
Lukas Haase <lukas.haase@mni.thm.de>
2019-09-09 12:30:14 +0200

    Input Cursor, HIGH CONTRAST THEME

---
+++ src/theme/high-contrast-theme/highContrastTheme.const.ts
@@ -0,0 +1,27 @@
+export const highcontrast = {
+
+  '--primary' : '#fb9a1c',
+  '--primary-variant': '#1e1e1e',
+
+  '--secondary': '#fb9a1c',
+  '--secondary-variant': '#fb9a1c',
+
+  '--background': '#1e1e1e',
+  '--surface': '#141414',
+  '--dialog': '#fb9a1c',
+
+  '--on-primary': '#141414',
+  '--on-secondary': '#FFFFFF',
+  '--on-background': '#FFFFFF',
+  '--on-surface': '#FFFFFF',
+
+  '--green': 'green',
+  '--red': 'red',
+  '--yellow': 'yellow',
+  '--blue': 'blue',
+  '--purple': 'purple',
+  '--light-green': 'lightgreen',
+  '--grey': 'grey',
+  '--grey-light': 'lightgrey',
+  '--black': 'black'
+};
