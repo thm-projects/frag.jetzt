b5ece9b3cad54f26c953a77f257cf364463223cf
Julia Langhammer <julia.langhammer@mni.thm.de>
2019-09-09 15:48:40 +0200

    Resolve "Automatic hyphenation for all texts"

--- src/assets/i18n/cookies/cookies-de.ts
+++ src/assets/i18n/cookies/cookies-de.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cookies-de',
-  templateUrl: './cookies-de.html'
+  templateUrl: './cookies-de.html',
+  styleUrls: ['./cookies.scss']
 })
 
 export class CookiesDeComponent {}
