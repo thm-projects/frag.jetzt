4d7fda90ee6b11797ec1009418b83458ee9a0d8c
Marco Herzog <marco.herzog@mni.thm.de>
2019-09-11 10:56:50 +0200

    Fix circular dependency between cookies and overlay

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -19,7 +19,7 @@ import { OverlayComponent } from '../../home/_dialogs/overlay/overlay.component'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
 
