2bc239552cbc4548ea87a88fbc1a429d21bdb812
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-10-04 13:57:03 +0200

    Login after session creation dialog is accepted

--- src/app/components/home/new-landing/new-landing.component.ts
+++ src/app/components/home/new-landing/new-landing.component.ts
@@ -4,9 +4,6 @@ import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '../../../services/util/language.service';
-import { AuthenticationService } from '../../../services/http/authentication.service';
-import { User } from '../../../models/user';
-import { UserRole } from '../../../models/user-roles.enum';
 
 @Component({
   selector: 'app-new-landing',
@@ -15,29 +12,15 @@ import { UserRole } from '../../../models/user-roles.enum';
 })
 export class NewLandingComponent implements OnInit {
 
-  user: User;
-
   constructor(private router: Router,
               public dialog: MatDialog,
               private translateService: TranslateService,
-              protected langService: LanguageService,
-              public authenticationService: AuthenticationService) {
+              protected langService: LanguageService) {
     langService.langEmitter.subscribe(lang => translateService.use(lang));
   }
 
   ngOnInit() {
     this.translateService.use(localStorage.getItem('currentLang'));
-    this.authenticationService.watchUser.subscribe(newUser => this.user = newUser);
-  }
-
-  createSession() {
-    if (!this.user) {
-      this.authenticationService.guestLogin(UserRole.CREATOR).subscribe( () => {
-        this.openCreateRoomDialog();
-      });
-    } else {
-      this.openCreateRoomDialog();
-    }
   }
 
   openCreateRoomDialog(): void {
