41dd6569b901c0dba962d95aca8eb275912049ac
Tom Käsler <tom.kaesler@mni.thm.de>
2019-10-10 20:47:34 +0200

    Add commentId to bonus token

--- src/app/models/bonus-token.ts
+++ src/app/models/bonus-token.ts
@@ -1,14 +1,17 @@
 export class BonusToken {
   roomId: string;
+  commentId: string;
   userId: string;
   token: string;
 
   constructor(
     roomId: string,
+    commentId: string,
     userId: string,
     token: string
   ) {
     this.roomId = roomId;
+    this.commentId = commentId;
     this.userId = userId;
     this.token = token;
   }
