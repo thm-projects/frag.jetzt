c3eecc44f12e3901366f7951ce958015cf3b1246
Lukas Mauß <lukas.mauss@mni.thm.de>
2019-10-16 18:30:56 +0200

    Resolve "Bonus questions should not be able to be deleted"

--- src/app/components/creator/_dialogs/delete-comments/delete-comments.component.ts
+++ src/app/components/creator/_dialogs/delete-comments/delete-comments.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { RoomEditComponent } from '../room-edit/room-edit.component';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { DialogConfirmActionButtonType } from '../../../shared/dialog/dialog-action-buttons/dialog-action-buttons.component';
 import { TranslateService } from '@ngx-translate/core';
+import { BonusTokenService } from '../../../../services/http/bonus-token.service';
+import { CommentSettingsComponent } from '../comment-settings/comment-settings.component';
 
 @Component({
   selector: 'app-delete-comment',
@@ -16,16 +17,25 @@ export class DeleteCommentsComponent implements OnInit {
    * The confirm button type of the dialog.
    */
   confirmButtonType: DialogConfirmActionButtonType = DialogConfirmActionButtonType.Alert;
+  roomId: string;
+  bonusQuestions = false;
 
-  constructor(public dialogRef: MatDialogRef<RoomEditComponent>,
+  constructor(public dialogRef: MatDialogRef<CommentSettingsComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               private liveAnnouncer: LiveAnnouncer,
-              private translationService: TranslateService )  { }
+              private translationService: TranslateService,
+              private tokenService: BonusTokenService)  { }
 
 
   ngOnInit() {
     this.translationService.get('room-page.really-delete-comments').subscribe(msg => {
       this.liveAnnouncer.announce(msg);
+    });
+    this.tokenService.getTokensByRoomId(this.roomId).subscribe(tokens => {
+      if (tokens.length > 0) {
+        console.log('LEL EHY HEREWÄ');
+        this.bonusQuestions = true;
+      }
     });
   }
 
