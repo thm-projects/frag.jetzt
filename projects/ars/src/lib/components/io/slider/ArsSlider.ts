0f32f4eeb0ae4e61bc8ef5ce2b82afcec028a436
= <lukas.haase@mni.thm.de>
2019-11-04 11:15:17 +0100

    button design fix

---
+++ projects/ars/src/lib/components/io/slider/ArsSlider.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[ars-slider]'
+})
+// tslint:disable-next-line:directive-class-suffix
+export class ArsSlider {
+
+}
