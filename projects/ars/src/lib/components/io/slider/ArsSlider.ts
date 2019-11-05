614bc33b19557f961685452c617c542ba131d095
= <lukas.haase@mni.thm.de>
2019-11-05 10:45:12 +0100

    rework slider

--- projects/ars/src/lib/components/io/slider/ArsSlider.ts
+++ projects/ars/src/lib/components/io/slider/ArsSlider.ts
@@ -7,55 +7,69 @@ import { MatSlider } from '@angular/material';
   selector: '[ars-slider]'
 })
 // tslint:disable-next-line:directive-class-suffix
-export class ArsSlider implements OnInit,AfterViewInit {
+export class ArsSlider implements OnInit, AfterViewInit {
 
-  public static classes:Object={
-    'mat-slider-wrapper':'',
-    'mat-slider-track-wrapper':'rgba(127,127,127,0.5)',
-    'mat-slider-track-background':'',
-    'mat-slider-track-fill':'var(--on-surface)',
-    'mat-slider-ticks-container':'',
-    'mat-slider-ticks':'',
-    'mat-slider-focus-ring':'var(--on-surface)',
-    'mat-slider-thumb':'var(--on-surface)',
-    'mat-slider-thumb-container':'var(--on-surface)',
-    'mat-slider-thumb-label':'',
-    'mat-slider-thumb-label-text':''
+  public static classes: Object = {
+    'mat-slider-wrapper': '',
+    'mat-slider-track-wrapper': 'rgba(127,127,127,0.5)',
+    'mat-slider-track-background': '',
+    'mat-slider-track-fill': 'var(--on-surface)',
+    'mat-slider-ticks-container': '',
+    'mat-slider-ticks': '',
+    'mat-slider-focus-ring': 'var(--on-surface)',
+    'mat-slider-thumb': 'var(--on-surface)',
+    'mat-slider-thumb-container': 'var(--on-surface)',
+    'mat-slider-thumb-label': '',
+    'mat-slider-thumb-label-text': ''
   };
 
-  @Input() width:number;
+  @Input() width: number;
 
-  private elem:HTMLInputElement;
+  private elem: HTMLInputElement;
 
-  constructor(private slider:MatSlider, private render:Renderer2) {
+  constructor(private slider: MatSlider, private render: Renderer2) {
     this.elem = slider._elementRef.nativeElement;
   }
 
-  ngOnInit(){
+  ngOnInit() {
   }
 
-  ngAfterViewInit(){
-    Array.from(this.elem.getElementsByTagName('*')).forEach(e=>{
+  ngAfterViewInit() {
+    Array.from(this.elem.getElementsByTagName('*')).forEach(e => {
       console.log(e.className);
-      if(ArsSlider.classes.hasOwnProperty(e.className)){
-        (<HTMLElement>e).style.background=ArsSlider.classes[e.className];
+      if (ArsSlider.classes.hasOwnProperty(e.className)) {
+        (<HTMLElement>e).style.background = ArsSlider.classes[e.className];
       }
     });
-    this.render.setStyle(this.elem,'height','48px');
-    this.render.setStyle(this.elem,'minHeight','48px');
-    this.render.setStyle(this.elem,'maxHeight','48px');
+    this.render.setStyle(this.elem, 'height', '48px');
+    this.render.setStyle(this.elem, 'minHeight', '48px');
+    this.render.setStyle(this.elem, 'maxHeight', '48px');
     this.updateWidth();
   }
 
-  public setWidth(width:number){
-    this.width=width;
+  public setWidth(width: number) {
+    this.width = width;
     this.updateWidth();
   }
 
-  private updateWidth(){
-    this.render.setStyle(this.elem,'width',this.width+'px');
-    this.render.setStyle(this.elem,'minWidth',this.width+'px');
-    this.render.setStyle(this.elem,'maxWidth',this.width+'px');
+  private updateWidth() {
+    this.render.setStyle(this.elem, 'width', this.width + 'px');
+    this.render.setStyle(this.elem, 'minWidth', this.width + 'px');
+    this.render.setStyle(this.elem, 'maxWidth', this.width + 'px');
+  }
+
+  public next() {
+    this.slider.value += this.slider.step;
+    if (this.slider.value > this.slider.max) {
+      this.slider.value = this.slider.max;
+    }
+  }
+
+  public prev() {
+    this.slider.value -= this.slider.step;
+    if (this.slider.value < this.slider.min) {
+      this.slider.value = this.slider.min;
+    }
   }
 
 }
--- projects/ars/src/lib/components/io/slider/ars-slider-comb/ars-slider-comb.component.ts
+++ projects/ars/src/lib/components/io/slider/ars-slider-comb/ars-slider-comb.component.ts
@@ -1,35 +1,29 @@
-import { AfterViewInit, Component, ContentChild, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { ArsSlider } from '../ArsSlider';
-import { MatIcon } from '@angular/material';
 
 @Component({
   selector: 'ars-slider-comb',
   templateUrl: './ars-slider-comb.component.html',
   styleUrls: ['./ars-slider-comb.component.scss']
 })
-export class ArsSliderCombComponent implements OnInit,AfterViewInit {
+export class ArsSliderCombComponent implements OnInit, AfterViewInit {
 
-  @Input() width:number;
-  @Input() leftIcon:string='keyboard_arrow_left';
-  @Input() rightIcon:string='keyboard_arrow_right';
-  @ContentChild(ArsSlider) slider:ArsSlider;
+  @Input() width: number;
+  @Input() leftIcon = 'keyboard_arrow_left';
+  @Input() rightIcon = 'keyboard_arrow_right';
+  @ContentChild(ArsSlider) slider: ArsSlider;
 
-  constructor(private ref:ElementRef, private render:Renderer2) { }
+  constructor(private ref: ElementRef, private render: Renderer2) {
+  }
 
   ngOnInit() {
   }
 
-  ngAfterViewInit(){
-    this.slider.setWidth(this.width-68);
-    if(this.width){
-      this.render.setStyle(this.ref.nativeElement,'width',this.width+'px');
+  ngAfterViewInit() {
+    this.slider.setWidth(this.width - 68);
+    if (this.width) {
+      this.render.setStyle(this.ref.nativeElement, 'width', this.width + 'px');
     }
-  }
-
-  private prev(){
-  }
-
-  private next(){
   }
 
 }
