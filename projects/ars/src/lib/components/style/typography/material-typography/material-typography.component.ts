93ec7e9af52c10409ac37a81b2ce9353b25b8b7e
= <lukas.haase@mni.thm.de>
2019-12-02 12:33:19 +0100

    ars-style-typography-material component

---
+++ projects/ars/src/lib/components/style/typography/material-typography/material-typography.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'ars-style-typography-material',
+  templateUrl: './material-typography.component.html',
+  styleUrls: ['./material-typography.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MaterialTypographyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
