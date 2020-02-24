3bd4cd74010dc32a950fb539a0fa392038d0e807
Lukas Mauß <lukas.mauss@mni.thm.de>
2020-02-24 20:52:15 +0100

    Add 'mailto'-function for users bonus tokens

--- src/app/components/participant/_dialogs/user-bonus-token/user-bonus-token.component.ts
+++ src/app/components/participant/_dialogs/user-bonus-token/user-bonus-token.component.ts
@@ -5,6 +5,7 @@ import { BonusToken } from '../../../../models/bonus-token';
 import { BonusTokenRoomMixin } from '../../../../models/bonus-token-room-mixin';
 import { MatDialogRef } from '@angular/material';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-bonus-token',
@@ -15,12 +16,14 @@ export class UserBonusTokenComponent implements OnInit {
   userId: string;
   bonusTokens: BonusToken[] = [];
   bonusTokensMixin: BonusTokenRoomMixin[] = [];
+  currentBT: BonusTokenRoomMixin;
 
   constructor(
     private bonusTokenService: BonusTokenService,
     private roomService: RoomService,
     private dialogRef: MatDialogRef<UserBonusTokenComponent>,
-    protected router: Router
+    protected router: Router,
+    public translationService: TranslateService
   ) {
   }
 
@@ -44,6 +47,34 @@ export class UserBonusTokenComponent implements OnInit {
     this.dialogRef.close();
     const commentURL = `participant/room/${this.bonusTokensMixin[index].roomShortId}/comment/${this.bonusTokens[index].commentId}`;
     this.router.navigate([commentURL]);
+  }
+
+  getTokensByRoom(shortId: string): string {
+    let tokens = '';
+    for (const bt of this.bonusTokensMixin) {
+      if (bt.roomShortId === shortId) {
+        tokens += bt.token + '%0D%0A%0D%0A';
+      }
+    }
+    return tokens;
+  }
+
+  setCurrentRoom(bt: BonusTokenRoomMixin) {
+    this.currentBT = bt;
+  }
+
+  openMail() {
+    const sessionName = this.currentBT.roomName;
+    const sessionId = this.currentBT.roomShortId;
+    const translationList = ['user-bonus-token.mail-subject', 'user-bonus-token.mail-body-1', 'user-bonus-token.mail-body-2',
+      'user-bonus-token.mail-body-3', 'user-bonus-token.mail-body-4'];
+    let mailText: string;
+    this.translationService.get(translationList).subscribe(msgs => {
+      console.log(msgs);
+      mailText = 'mailto:?subject=' + msgs[translationList[0]] + sessionName + '%C2%AB&body=' + msgs[translationList[1]] + sessionName
+      + msgs[translationList[2]] + sessionId + msgs[translationList[3]] + this.getTokensByRoom(sessionId) + msgs[translationList[4]];
+      window.location.href = mailText;
+    });
   }
 
   /**
