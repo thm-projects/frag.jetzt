5633985ec3e1f1726c5a56e99d51bfcdc861729a
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2020-08-03 14:16:11 +0200

    mostly wording

--- projects/ars/src/lib/style/style.const.ts
+++ projects/ars/src/lib/style/style.const.ts
@@ -4,8 +4,8 @@ export const light = {
   'ars-button-color': '#000000',
   'ars-button-color-light': '#000000',
   'ars-background-color': 'Moccasin',
-  'ars-foreground-color': 'lightgray',
-  'ars-border-color': 'darkgray'
+  'ars-foreground-color': '#fffde7',
+  'ars-border-color': '#fffde7'
 };
 
 export const dark = {
