6ad4058d2f6d5ce1d0a0e3e9816cdcd59eeee19d
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2020-08-26 07:59:20 +0200

    wording

--- src/theme/light-theme/light-theme.ts
+++ src/theme/light-theme/light-theme.ts
@@ -11,7 +11,7 @@ export const arsnova = {
   '--dialog': 'LightGoldenrodYellow',
   '--cancel': 'Chocolate',
   '--alt-surface': '#eeeeee',
-  '--alt-dialog': '#eeeeee',
+  '--alt-dialog': 'Moccasin',
 
   '--on-primary': '#ffffff',
   '--on-secondary': '#FFFFFF',
