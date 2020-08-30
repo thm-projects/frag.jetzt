02181016afb1cc25bdac1b7c9008c8a257175c3c
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2020-08-30 09:17:07 +0200

    set question wall colors to light mode

--- projects/ars/src/lib/style/style.const.ts
+++ projects/ars/src/lib/style/style.const.ts
@@ -9,11 +9,11 @@ export const light = {
 };
 
 export const dark = {
-  'ars-header-color': '#FAFAFA',
-  'ars-paragraph-color': '#BDBDBD',
-  'ars-button-color': '#FFFFFF',
-  'ars-button-color-light': '#909090',
-  'ars-background-color': '#212121',
-  'ars-foreground-color': '#292929',
-  'ars-border-color': '#2E2E2E'
+  'ars-header-color': '#212121',
+  'ars-paragraph-color': '#3f3f3f',
+  'ars-button-color': '#000000',
+  'ars-button-color-light': '#000000',
+  'ars-background-color': 'Moccasin',
+  'ars-foreground-color': '#fffde7',
+  'ars-border-color': '#fffde7'
 };
