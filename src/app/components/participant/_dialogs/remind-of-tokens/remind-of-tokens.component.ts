c8edfae9285095cc824f5e84bc36ab7ca7f8b8dd
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel@mni.thm.de>
2020-09-03 16:14:45 +0200

    back to icon buttons

--- src/app/components/participant/_dialogs/remind-of-tokens/remind-of-tokens.component.ts
+++ src/app/components/participant/_dialogs/remind-of-tokens/remind-of-tokens.component.ts
@@ -5,13 +5,13 @@ import { RoomEditComponent } from '../../../creator/_dialogs/room-edit/room-edit
 @Component({
   selector: 'app-remind-of-tokens',
   templateUrl: './remind-of-tokens.component.html',
-  styleUrls: ['./remind-of-tokens.component.scss']
+  styleUrls: ['./remind-of-tokens.component.scss'],
 })
 export class RemindOfTokensComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<RoomEditComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
   ) {
   }
 
