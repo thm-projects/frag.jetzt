3025e5d96c7ffef24a770c04bef8ebe9efc5fcc0
Stefan Plociennik <stefan.plociennik@mni.thm.de>
2021-04-28 21:11:12 +0200

    add mat-expansion-panel to Dialog component; created some example tags with questions

--- src/app/components/shared/topic-cloud-dialog/topic-cloud-dialog.component.ts
+++ src/app/components/shared/topic-cloud-dialog/topic-cloud-dialog.component.ts
@@ -7,13 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicCloudDialogComponent implements OnInit {
 
-  public panelOpenState: boolean;
-  public keywords: {
-    "Cloud",
-    "SWTP",
-    "Frage",
-    "Klausur"
-  };
+  public panelOpenState = false;
+  public array = [
+    {
+      "keyword": "Cloud",
+      "correspondingQuestions": [
+        "Wie genau ist die Cloud aufgebaut?",
+        "Wieviel speicherplatz steht mir in der Cloud zur verfuegung?",
+        "Sollen wir die Tag Cloud implementieren?"
+      ]
+    },
+    {
+      "keyword": "SWT",
+      "correspondingQuestions": [
+        "Muss man fuer das Modul SWT bestanden haben?"
+      ]
+    },
+    {
+      "keyword": "Frage",
+      "correspondingQuestions": [
+        "Das ist eine Lange Frage mit dem Thema 'frage'",
+        "Ich habe eine Frage, sind Fragen zum thema 'Frage' auch erlaubt?",
+        "Ich wollte Fragen ob sie gerne Sachen gefragt werden",
+        "Langsam geht mir die Fragerei mit den ganzen Fragen auf den Geist"
+      ]
+    },
+    {
+      "keyword": "Klausur",
+      "correspondingQuestions": [
+        "Darf man in der Klausur hilfmittel verwenden?",
+        "An welchem Termin findet die Klausur statt?"
+      ]
+    }
+  ];
 
   constructor() { }
 
