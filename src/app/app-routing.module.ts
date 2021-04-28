122342da48ce71e911311d43c3ae62b0ff4e715f
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2021-04-28 17:57:52 +0200

    Executed second angular update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [
     RouterModule
