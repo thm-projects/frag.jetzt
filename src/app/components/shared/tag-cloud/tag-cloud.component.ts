601f274c1be3ea75581c37c2b96027bf3637342f
Fabian Gruenwald <fabian.gruenwald@mni.thm.de>
2021-05-01 17:09:44 +0200

    commit

--- src/app/components/shared/tag-cloud/tag-cloud.component.ts
+++ src/app/components/shared/tag-cloud/tag-cloud.component.ts
@@ -5,7 +5,6 @@ import {CommentService} from '../../../services/http/comment.service';
 import {Result, SpacyService} from '../../../services/http/spacy.service';
 import {Comment} from '../../../models/comment';
 
-const COLOR: string[] = ['blue', 'red', 'yellow', 'green'];
 
 class TagComment implements CloudData {
   constructor(public color: string,
@@ -17,6 +16,20 @@ class TagComment implements CloudData {
               public tooltip: string,
               public weight: number) {
   }
+}
+
+
+const weight2color = {
+  1: "blue",
+  2: "green",
+  3: "yellow",
+  4: "orange",
+  5: "pink",
+  6: "gray",
+  7: "lightgreen",
+  8: "tomato",
+  9: "white",
+  10: "brown"
 }
 
 @Component({
@@ -34,11 +47,13 @@ export class TagCloudComponent implements OnInit {
     // if height is between 0 and 1 it will be set to the height of the upper element multiplied by the value
     height: 1,
     overflow: false,
+    font: "Georgia" // not working
   };
   zoomOnHoverOptions: ZoomOnHoverOptions = {
-    scale: 1.3, // Elements will become 130 % of current zize on hover
+    scale: 1.3, // Elements will become 130 % of current size on hover
     transitionTime: 0.6, // it will take 0.6 seconds until the zoom level defined in scale property has been reached
-    delay: 0.4 // Zoom will take affect after 0.8 seconds
+    delay: 0.4,// Zoom will take affect after 0.4 seconds
+    color: "red"
   };
 
   data: CloudData[] = [];
@@ -55,20 +70,21 @@ export class TagCloudComponent implements OnInit {
     });
   }
 
-
   analyse(comments: Comment[]) {
     const commentsConcatenated = comments.map(c => c.body).join(' ');
 
     this.spacyService.analyse(commentsConcatenated, 'de').subscribe((res: Result) => {
 
-        this.data = res.words.filter(w => ['NE', 'NN', 'NMP', 'NNE'].indexOf(w.tag) >= 0).map(w =>
-          new TagComment(COLOR[Math.floor(
-            Math.random() * (COLOR.length - 1))],
-            true, null, null,
-            Math.floor(Math.random() * 30 - 15), w.text,
-            'todo', Math.floor(Math.random() * 10) + 1)
+        this.data = res.words.filter(w => ['NE', 'NN', 'NMP', 'NNE'].indexOf(w.tag) >= 0).map(w => {
+            const weight = 5 + Math.floor(Math.random() * 4 + 1);
+            const color = weight2color[weight];
+            console.log(weight, color)
+            return new TagComment(color,
+              true, null, null,
+              /*Math.floor(Math.random() * 30 - 15)*/0, w.text,
+              'TODO', weight)
+          }
         );
-
       }
     );
   }
