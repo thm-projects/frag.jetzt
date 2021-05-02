a812128ac4e627b702491d97b333e06ce354fd26
Tom Frederik Leimbrock <tom.leimbrock@mni.thm.de>
2021-05-02 12:25:02 +0200

    removed test-cload

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -30,7 +30,6 @@ import { MotdDialogComponent } from './_dialogs/motd-dialog/motd-dialog.componen
 import { MotdMessageComponent } from './_dialogs/motd-dialog/motd-message/motd-message.component';
 import { CloudConfigurationComponent } from "./_dialogs/cloud-configuration/cloud-configuration.component";
 import { TagCloudModule } from 'angular-tag-cloud-module';
-import { TagCloadComponent } from './tag-cload/tag-cload.component';
 import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
@@ -68,8 +67,7 @@ import { ColorPickerModule } from 'ngx-color-picker';
     RemoveFromHistoryComponent,
     CommentAnswerComponent,
     MotdDialogComponent,
-    MotdMessageComponent,
-    TagCloadComponent
+    MotdMessageComponent
   ],
   exports: [
     RoomJoinComponent,
