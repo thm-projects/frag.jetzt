b10c021691e80a6cad5cde3a637b40cdbe383c70
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2021-05-15 17:59:42 +0200

    Update interface for tag cloud configuration
    This commit is partial and does not support full functionality.
    The limitation of the tags within the cloud does not work yet.
    
    [Ticket: #32]

--- src/app/components/shared/tag-cloud/tag-cloud.interface.ts
+++ src/app/components/shared/tag-cloud/tag-cloud.interface.ts
@@ -4,6 +4,32 @@ export interface TagCloudHeaderDataOverview {
   tagCount: number;
 }
 
+export type CloudWeightCount = [
+  number, // w1
+  number, // w2
+  number, // w3
+  number, // w4
+  number, // w5
+  number, // w6
+  number, // w7
+  number, // w8
+  number, // w9
+  number // w10
+];
+
+export type CloudWeightColor = [
+  string, // w1
+  string, // w2
+  string, // w3
+  string, // w4
+  string, // w5
+  string, // w6
+  string, // w7
+  string, // w8
+  string, // w9
+  string // w10
+];
+
 export interface CloudParameters {
   /**
    * Background color of the Tag-cloud
@@ -41,4 +67,14 @@ export interface CloudParameters {
    * Enables random angles
    */
   randomAngles: boolean;
+  /**
+   * The count of cloud weights is used to limit the size of the displayed weighted elements.
+   *
+   * A number less than zero means that all elements of the weight are displayed.
+   */
+  cloudWeightCount: CloudWeightCount;
+  /**
+   * This array contains the CSS color property for each cloud weight
+   */
+  cloudWeightColor: CloudWeightColor;
 }
