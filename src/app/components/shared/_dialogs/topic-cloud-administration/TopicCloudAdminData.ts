72a08045c26fc662803ebe69ba4099d581a87232
Stefan Plociennik <stefan.plociennik@mni.thm.de>
2021-05-23 18:42:34 +0200

    removed unused code from data interface

--- src/app/components/shared/_dialogs/topic-cloud-administration/TopicCloudAdminData.ts
+++ src/app/components/shared/_dialogs/topic-cloud-administration/TopicCloudAdminData.ts
@@ -3,11 +3,4 @@ export interface TopicCloudAdminData{
     considerVotes: boolean;
     profanityFilter: boolean;
     hideBlacklist: boolean;
-    // constructor(private bList: string[], private cVotes: boolean,
-    //             private pFilter: boolean, private hBlacklist: boolean){
-    //     this.blackList = bList;
-    //     this.considerVotes = cVotes;
-    //     this.profanityFilter = pFilter;
-    //     this.hideBlacklist = hBlacklist;
-    // }
 }
