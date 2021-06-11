912dfee4214622ff9e1e03d5a10d4eaa22ed2c59
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2021-06-11 13:34:32 +0200

    Adds fields to tag cloud interface

--- src/app/services/http/spacy.service.ts
+++ src/app/services/http/spacy.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BaseHttpService } from './base-http.service';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 export type Model = 'de' | 'en' | 'fr';
 
@@ -33,6 +33,19 @@ export class Word {
   ) {
     this.tag = tag;
     this.text = text;
+  }
+}
+
+export class Noun {
+  text: string;
+  dependencyRelation: string;
+
+  constructor(
+    text: string,
+    dependencyRelation: string
+  ) {
+    this.text = text;
+    this.dependencyRelation = dependencyRelation;
   }
 }
 
@@ -74,7 +87,32 @@ export class SpacyService extends BaseHttpService {
   }
 
   getKeywords(text: string, model: string): Observable<string[]> {
+    console.log(text);
     return this.analyse(text, model).pipe(
+      tap(e => {
+        const nouns: Noun[] = [];
+        const absoluteNouns = e.words.filter((v, index) => {
+          v['index'] = index;
+          return v.tag.charAt(0) === 'N';
+        });
+        console.log(e);
+        for (const arc of e.arcs) {
+          const index = arc.dir.charAt(0) === 'r' ? arc.end : arc.start;
+          const elem = e.words[index];
+          if (elem.tag.charAt(0) === 'N') {
+            while (absoluteNouns[0]['index'] < index) {
+              const current = absoluteNouns.splice(0, 1)[0];
+              console.log(current);
+              nouns.push(new Noun(current['text'], 'ROOT'));
+            }
+            const actualNode = absoluteNouns.splice(0, 1)[0];
+            console.log(actualNode);
+            console.assert(actualNode['index'] === index, 'Indices should be equal!');
+            nouns.push(new Noun(elem.text, arc.label));
+          }
+        }
+        console.log(nouns);
+      }),
       map(result => result.words.filter(v => v.tag.charAt(0) === 'N').map(v => v.text))
     );
   }
