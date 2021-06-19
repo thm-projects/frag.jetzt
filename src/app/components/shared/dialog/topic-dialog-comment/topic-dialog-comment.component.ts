0f224b46046aea8bd60b3d3cd9fd47fb229c6426
mohammad <mohammad.alayoub@mni.thm.de>
2021-06-19 13:20:16 +0200

    simplify return statements; fix error if keyword includes illegal charachter for regular expressions

--- src/app/components/shared/dialog/topic-dialog-comment/topic-dialog-comment.component.ts
+++ src/app/components/shared/dialog/topic-dialog-comment/topic-dialog-comment.component.ts
@@ -25,30 +25,28 @@ export class TopicDialogCommentComponent implements OnInit {
   constructor(private topicCloudAdminService: TopicCloudAdminService) {}
 
   get partsOfQuestion() {
-    if (this.profanityFilter) {
-      return this.partsWithoutProfanity;
-    } else {
-      return this.parts;
-    }
+    return this.profanityFilter ? this.partsWithoutProfanity : this.parts;
   }
 
   get partsOfShortQuestion(){
-    if (this.profanityFilter) {
-      return this.partsWithoutProfanityShort;
-    } else {
-      return this.partsShort;
-    }
+    return this.profanityFilter ? this.partsWithoutProfanityShort : this.partsShort;
   }
 
-  shortQuestionParts(question: string){
-    return question.slice(0, this.maxShowedCharachters).split(new RegExp(this.keyword, 'i'));
+  splitShortQuestion(question: string){
+    const cleanedKeyword = this.keyword.replace(/([^a-z0-9]+)/gi, '');
+    return question.slice(0, this.maxShowedCharachters).split(new RegExp(cleanedKeyword, 'i'));
+  }
+
+  splitQuestion(question: string){
+    const cleanedKeyword = this.keyword.replace(/([^a-z0-9]+)/gi, '');
+    return question.split(new RegExp(cleanedKeyword,'i'));
   }
 
   ngOnInit(): void {
     this.questionWithoutProfanity = this.topicCloudAdminService.filterProfanityWords(this.question);
-    this.partsWithoutProfanity = this.questionWithoutProfanity.split(new RegExp(this.keyword,'i'));
-    this.partsWithoutProfanityShort = this.shortQuestionParts(this.questionWithoutProfanity);
-    this.parts = this.question.split(new RegExp(this.keyword,'i'));
-    this.partsShort = this.shortQuestionParts(this.question);
+    this.partsWithoutProfanity = this.splitQuestion(this.questionWithoutProfanity);
+    this.parts = this.splitQuestion(this.question);
+    this.partsWithoutProfanityShort = this.splitShortQuestion(this.questionWithoutProfanity);
+    this.partsShort = this.splitShortQuestion(this.question);
   }
 }
