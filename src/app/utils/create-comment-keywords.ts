6f9283d1357fc66672542b5daba5f5d9d8716295
Lars Wächter <lars.waechter@mni.thm.de>
2021-06-26 18:47:58 +0200

    Fix cleaning regex

--- src/app/utils/create-comment-keywords.ts
+++ src/app/utils/create-comment-keywords.ts
@@ -23,7 +23,7 @@ export class CreateCommentKeywords {
     const regexEmoji    = new RegExp('\uD918\uDD28|\ufe0f|\u200D|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]', 'g');
     const regexKatex    = new RegExp('\\$[^$\\n ]+\\$|\\$\\$[^$\\n ]+\\$\\$','g');
     const regexMarkdown = new RegExp('(?:__|[*#])|\\[(.+?)]\\((.+?)\\)', 'g');
-    const regexBlank    = new RegExp('[\\s]{2,}', 'g');
+    const regexBlank    = new RegExp('[\\s]{2,}', 'gu');
     text = text.replace(regexKatex,'');
     text = text.replace(regexEmoji,'');
     text = text.replace(regexMarkdown,'');
