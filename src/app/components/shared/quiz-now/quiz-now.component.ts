7d58f32b8592e177a8cb15b8f8c5a9031d95d3e8
Philipp Sautner <philipp.sautner@hotmail.com>
2021-07-21 12:28:46 +0200

    Reverts the domSanitizing changes in favour of chaning the route to /quiz

--- src/app/components/shared/quiz-now/quiz-now.component.ts
+++ src/app/components/shared/quiz-now/quiz-now.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
 @Component({
   selector: 'app-quiz-now',
   templateUrl: './quiz-now.component.html',
   styleUrls: ['./quiz-now.component.scss']
 })
-export class QuizNowComponent implements OnInit {
-
-  url = 'https://arsnova.click';
-  urlSafe: SafeResourceUrl;
-
-  constructor(public sanitizer: DomSanitizer) { }
-
-  ngOnInit(): void {
-    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-  }
-
+export class QuizNowComponent {
 }
