ba21454ea3ca003653a0ff6274f9f0b1d5d7b7c7
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2021-09-29 19:41:48 +0200

    Implement name edit

---
+++ src/app/components/creator/_dialogs/room-name-settings/room-name-settings.component.spec.ts
@@ -0,0 +1,26 @@
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RoomNameSettingsComponent } from './room-name-settings.component';
+
+describe('RoomNameSettingsComponent', () => {
+  let component: RoomNameSettingsComponent;
+  let fixture: ComponentFixture<RoomNameSettingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RoomNameSettingsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoomNameSettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+ */
---
+++ src/app/components/creator/_dialogs/room-name-settings/room-name-settings.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Room } from '../../../../models/room';
+import { FormControl, Validators } from '@angular/forms';
+import { EventService } from '../../../../services/util/event.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { RoomCreatorPageComponent } from '../../room-creator-page/room-creator-page.component';
+import { RoomService } from '../../../../services/http/room.service';
+
+@Component({
+  selector: 'app-room-name-settings',
+  templateUrl: './room-name-settings.component.html',
+  styleUrls: ['./room-name-settings.component.scss']
+})
+export class RoomNameSettingsComponent implements OnInit {
+
+  @Input() editRoom: Room;
+  roomNameFormControl = new FormControl('', [
+    Validators.required, Validators.minLength(3), Validators.maxLength(20)
+  ]);
+
+  constructor(private dialogRef: MatDialogRef<RoomCreatorPageComponent>,
+              public eventService: EventService,
+              private roomService: RoomService) {
+  }
+
+  ngOnInit() {
+    this.roomNameFormControl.setValue(this.editRoom.name);
+  }
+
+  buildCloseDialogActionCallback(): () => void {
+    return () => this.dialogRef.close('abort');
+  }
+
+  buildConfirmDialogActionCallback(): () => void {
+    return () => this.save();
+  }
+
+  private save(): void {
+    if (!this.roomNameFormControl.hasError('required')
+      && !this.roomNameFormControl.hasError('minlength')
+      && !this.roomNameFormControl.hasError('maxlength')) {
+      this.editRoom.name = this.roomNameFormControl.value;
+      this.roomService.updateRoom(this.editRoom).subscribe(r => this.editRoom = r);
+      this.dialogRef.close('update');
+    }
+  }
+}
