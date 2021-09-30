3cab2e8496ccbd55a0cb651890f9338f6d343959
Lukas Haase <lukas.haase@mni.thm.de>
2021-09-30 18:53:01 +0200

    unify room-pages

--- src/app/components/moderator/moderator-comment-page/moderator-comment-page.component.ts
+++ src/app/components/moderator/moderator-comment-page/moderator-comment-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, OnDestroy, AfterContentInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, EventEmitter, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../../models/user';
 import { NotificationService } from '../../../services/util/notification.service';
@@ -7,65 +7,137 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { EventService } from '../../../services/util/event.service';
 import { KeyboardUtils } from '../../../utils/keyboard';
 import { KeyboardKey } from '../../../utils/keyboard/keys';
+import { Room } from '../../../models/room';
+import { HeaderService } from '../../../services/util/header.service';
+import { RoomService } from '../../../services/http/room.service';
+import { ArsComposeService } from '../../../../../projects/ars/src/lib/services/ars-compose.service';
+import { RoomPageEdit } from '../../shared/room-page/room-page-edit/room-page-edit';
+import { CommentSettingsDialog } from '../../../models/comment-settings-dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+import { Location } from '@angular/common';
+import { CommentService } from '../../../services/http/comment.service';
+import { BonusTokenService } from '../../../services/http/bonus-token.service';
+import { RoomPageComponent } from '../../shared/room-page/room-page.component';
+import { WsCommentService } from '../../../services/websockets/ws-comment.service';
 
 @Component({
-  selector: 'app-moderator-comment-page',
-  templateUrl: './moderator-comment-page.component.html',
-  styleUrls: ['./moderator-comment-page.component.scss']
+  selector:'app-moderator-comment-page',
+  templateUrl:'./moderator-comment-page.component.html',
+  styleUrls:['./moderator-comment-page.component.scss']
 })
-export class ModeratorCommentPageComponent implements OnInit, OnDestroy, AfterContentInit {
+export class ModeratorCommentPageComponent extends RoomPageComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit{
   roomId: string;
   user: User;
+  room: Room;
+  onDestroyListener: EventEmitter<void> = new EventEmitter<void>();
+  onAfterViewInitListener: EventEmitter<void> = new EventEmitter<void>();
+  onInitListener: EventEmitter<void> = new EventEmitter<void>();
+  roomPageEdit: RoomPageEdit;
+  viewModuleCount = 1;
 
   listenerFn: () => void;
 
-  constructor(private route: ActivatedRoute,
+  constructor(protected route: ActivatedRoute,
               private notification: NotificationService,
               private authenticationService: AuthenticationService,
               public eventService: EventService,
               private _r: Renderer2,
-              private liveAnnouncer: LiveAnnouncer) { }
+              private liveAnnouncer: LiveAnnouncer,
+              public headerService: HeaderService,
+              public roomService: RoomService,
+              public composeService: ArsComposeService,
+              public dialog: MatDialog,
+              public translationService: TranslateService,
+              public location: Location,
+              public commentService: CommentService,
+              public bonusTokenService: BonusTokenService,
+              public wsCommentService: WsCommentService){
+    super(roomService, route, location, wsCommentService, commentService, eventService);
+    this.roomPageEdit = new RoomPageEdit(
+      dialog,
+      translationService,
+      notification,
+      roomService,
+      eventService,
+      location,
+      commentService,
+      bonusTokenService,
+      headerService,
+      composeService,
+      authenticationService,
+      route,
+      {
+        onInitListener:this.onInitListener,
+        onAfterViewInitListener:this.onAfterViewInitListener,
+        onDestroyListener:this.onDestroyListener,
+        updateCommentSettings(settings: CommentSettingsDialog){
+          this.updateCommentSettings(settings);
+        }
+      }
+    );
+  }
 
-  ngAfterContentInit(): void {
-    setTimeout( () => {
+  ngAfterContentInit(): void{
+    setTimeout(() => {
       document.getElementById('live_announcer-button').focus();
     }, 500);
   }
-  ngOnInit(): void {
+  
+  ngAfterViewInit(){
+    this.onAfterViewInitListener.emit();
+  }
+
+  ngOnInit(): void{
     this.roomId = localStorage.getItem('roomId');
     this.user = this.authenticationService.getUser();
     this.announce();
     this.listenerFn = this._r.listen(document, 'keyup', (event) => {
-      if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false) {
+      if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false){
         document.getElementById('searchBox').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit3) === true && this.eventService.focusOnInput === false) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit3) === true && this.eventService.focusOnInput === false){
         document.getElementById('sort-button').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit4) === true && this.eventService.focusOnInput === false) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit4) === true && this.eventService.focusOnInput === false){
         document.getElementById('filter-button').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit8) === true && this.eventService.focusOnInput === false) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit8) === true && this.eventService.focusOnInput === false){
         this.liveAnnouncer.announce('Aktueller Sitzungs-' + document.getElementById('shortId-header').textContent);
-      } else if (
+      }else if (
         KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit9, KeyboardKey.Escape) === true &&
-        this.eventService.focusOnInput === false) {
+        this.eventService.focusOnInput === false){
         this.announce();
-      } else if (
+      }else if (
         document.getElementById('search_close-button') &&
         KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true &&
-        this.eventService.focusOnInput === true) {
+        this.eventService.focusOnInput === true){
         document.getElementById('search_close-button').click();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true && this.eventService.focusOnInput === true) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true && this.eventService.focusOnInput === true){
         this.eventService.makeFocusOnInputFalse();
         document.getElementById('sort-button').focus();
       }
     });
+    this.onInitListener.emit();
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(){
     this.listenerFn();
     this.eventService.makeFocusOnInputFalse();
+    this.onDestroyListener.emit();
   }
 
-  public announce() {
+  updateCommentSettings(settings: CommentSettingsDialog){
+    this.room.tags = settings.tags;
+
+    if (this.moderationEnabled && !settings.enableModeration){
+      this.viewModuleCount = this.viewModuleCount - 1;
+    }else if (!this.moderationEnabled && settings.enableModeration){
+      this.viewModuleCount = this.viewModuleCount + 1;
+    }
+
+    this.moderationEnabled = settings.enableModeration;
+    localStorage.setItem('moderationEnabled', String(this.moderationEnabled));
+  }
+
+  public announce(){
     this.liveAnnouncer.clear();
     this.liveAnnouncer.announce('Du befindest dich auf der Moderations-Seite deiner Sitzung. ' +
       'Drücke die Taste 2 um auf das Sitzungs-Menü zu gelangen, ' +
--- src/app/components/participant/room-participant-page/room-participant-page.component.ts
+++ src/app/components/participant/room-participant-page/room-participant-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, EventEmitter, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Room } from '../../../models/room';
 import { User } from '../../../models/user';
 import { UserRole } from '../../../models/user-roles.enum';
@@ -17,18 +17,30 @@ import { KeyboardUtils } from '../../../utils/keyboard';
 import { KeyboardKey } from '../../../utils/keyboard/keys';
 import { map } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import { CommentSettingsDialog } from '../../../models/comment-settings-dialog';
+import { RoomPageEdit } from '../../shared/room-page/room-page-edit/room-page-edit';
+import { MatDialog } from '@angular/material/dialog';
+import { NotificationService } from '../../../services/util/notification.service';
+import { BonusTokenService } from '../../../services/http/bonus-token.service';
+import { HeaderService } from '../../../services/util/header.service';
+import { ArsComposeService } from '../../../../../projects/ars/src/lib/services/ars-compose.service';
 
 @Component({
-  selector: 'app-room-participant-page',
-  templateUrl: './room-participant-page.component.html',
-  styleUrls: ['./room-participant-page.component.scss']
+  selector:'app-room-participant-page',
+  templateUrl:'./room-participant-page.component.html',
+  styleUrls:['./room-participant-page.component.scss']
 })
-export class RoomParticipantPageComponent extends RoomPageComponent implements OnInit, OnDestroy, AfterContentInit {
+export class RoomParticipantPageComponent extends RoomPageComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit{
 
   room: Room;
   isLoading = true;
   deviceType = localStorage.getItem('deviceType');
   user: User;
+  viewModuleCount = 1;
+  roomPageEdit: RoomPageEdit;
+  onDestroyListener: EventEmitter<void> = new EventEmitter<void>();
+  onAfterViewInitListener: EventEmitter<void> = new EventEmitter<void>();
+  onInitListener: EventEmitter<void> = new EventEmitter<void>();
 
   constructor(protected location: Location,
               protected roomService: RoomService,
@@ -40,50 +52,87 @@ export class RoomParticipantPageComponent extends RoomPageComponent implements O
               protected authenticationService: AuthenticationService,
               private liveAnnouncer: LiveAnnouncer,
               private _r: Renderer2,
-              public eventService: EventService) {
+              public eventService: EventService,
+              public dialog: MatDialog,
+              public notification: NotificationService,
+              public bonusTokenService: BonusTokenService,
+              public headerService: HeaderService,
+              public composeService: ArsComposeService){
     super(roomService, route, location, wsCommentService, commentService, eventService);
     langService.langEmitter.subscribe(lang => translateService.use(lang));
+    this.roomPageEdit = new RoomPageEdit(
+      dialog,
+      translateService,
+      notification,
+      roomService,
+      eventService,
+      location,
+      commentService,
+      bonusTokenService,
+      headerService,
+      composeService,
+      authenticationService,
+      route,
+      {
+        onInitListener:this.onInitListener,
+        onAfterViewInitListener:this.onAfterViewInitListener,
+        onDestroyListener:this.onDestroyListener,
+        updateCommentSettings(settings: CommentSettingsDialog){
+          this.updateCommentSettings(settings);
+        }
+      }
+    );
   }
 
-  ngAfterContentInit(): void {
+  ngOnDestroy(){
+    super.ngOnDestroy();
+    this.onDestroyListener.emit();
+  }
+
+  ngAfterContentInit(): void{
     setTimeout(() => {
       document.getElementById('live_announcer-button').focus();
     }, 700);
   }
 
-  ngOnInit() {
+  ngAfterViewInit(){
+    this.onAfterViewInitListener.emit();
+  }
+
+  ngOnInit(){
     window.scroll(0, 0);
     this.route.params.subscribe(params => {
       this.initializeRoom(params['shortId']);
     });
     this.translateService.use(localStorage.getItem('currentLang'));
     this.listenerFn = this._r.listen(document, 'keyup', (event) => {
-      if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false) {
+      if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false){
         document.getElementById('question_answer-button').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit8) === true && this.eventService.focusOnInput === false) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit8) === true && this.eventService.focusOnInput === false){
         this.liveAnnouncer.clear();
         this.liveAnnouncer.announce('Aktueller Raum-Code:' + this.room.shortId);
-      } else if (
+      }else if (
         KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape, KeyboardKey.Digit9) === true && this.eventService.focusOnInput === false
-      ) {
+      ){
         this.announce();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true && this.eventService.focusOnInput === true) {
+      }else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true && this.eventService.focusOnInput === true){
         document.getElementById('question_answer-button').focus();
         this.eventService.makeFocusOnInputFalse();
       }
     });
+    this.onInitListener.emit();
   }
 
-  public announce() {
+  public announce(){
     this.liveAnnouncer.clear();
     const lang: string = this.translateService.currentLang;
-    if (lang === 'de') {
+    if (lang === 'de'){
       this.liveAnnouncer.announce('Du befindest dich in der Sitzung' + this.room.name +
         'mit dem Raum-Code' + this.room.shortId + '.' +
         'Drücke die Taste 1 um eine Frage zu stellen, die Taste 2 für das Sitzungs-Menü, ' +
         'die Taste 8 um den aktuellen Raum-Code zu hören, die Taste 0 um auf den Zurück-Button zu gelangen, ' +
         'oder die Taste 9 um diese Ansage zu wiederholen.', 'assertive');
-    } else {
+    }else{
       this.liveAnnouncer.announce('You have entered the session' + this.room.name + 'with the room code' + this.room.shortId
         + '.' + 'Press 0 to go back to the previous page, ' +
         '1 to ask a question, 2 for the session menu' +
@@ -92,19 +141,32 @@ export class RoomParticipantPageComponent extends RoomPageComponent implements O
 
   }
 
-  preRoomLoadHook(): Observable<any> {
+  preRoomLoadHook(): Observable<any>{
     this.authenticationService.watchUser.subscribe(user => this.user = user);
-    if (!this.user) {
+    if (!this.user){
       return this.authenticationService.guestLogin(UserRole.PARTICIPANT).pipe(map((user) => {
         return user;
       }));
-    } else {
+    }else{
       return of(this.user);
     }
   }
 
-  postRoomLoadHook() {
-    if (!this.authenticationService.hasAccess(this.room.shortId, UserRole.PARTICIPANT)) {
+  updateCommentSettings(settings: CommentSettingsDialog){
+    this.room.tags = settings.tags;
+
+    if (this.moderationEnabled && !settings.enableModeration){
+      this.viewModuleCount = this.viewModuleCount - 1;
+    }else if (!this.moderationEnabled && settings.enableModeration){
+      this.viewModuleCount = this.viewModuleCount + 1;
+    }
+
+    this.moderationEnabled = settings.enableModeration;
+    localStorage.setItem('moderationEnabled', String(this.moderationEnabled));
+  }
+
+  postRoomLoadHook(){
+    if (!this.authenticationService.hasAccess(this.room.shortId, UserRole.PARTICIPANT)){
       this.authenticationService.setAccess(this.room.shortId, UserRole.PARTICIPANT);
       this.roomService.addToHistory(this.room.id);
     }
