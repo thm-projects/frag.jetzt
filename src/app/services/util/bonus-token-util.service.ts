5f9c749859a92036f3473d8736293fa444aeaed9
Philipp Sautner <philipp.sautner@mni.thm.de>
2022-02-02 11:31:47 +0100

    Adds changes to sort questionNumber, console.log is right, sort() not working properly

---
+++ src/app/services/util/bonus-token-util.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CommentService } from '../http/comment.service';
+import { BonusTokenService } from '../http/bonus-token.service';
+import { BonusToken } from '../../models/bonus-token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BonusTokenUtilService {
+
+  constructor(
+      private commentService: CommentService,
+      private bonusTokenService: BonusTokenService
+  ) {}
+
+  setQuestionNumber(bts: BonusToken[]): BonusToken[] {
+        bts.forEach(bt => {
+            this.commentService.getComment(bt.commentId).subscribe(comment => {
+                bt.questionNumber = comment.number;
+            });
+        })
+        return bts;
+  }
+}
+
