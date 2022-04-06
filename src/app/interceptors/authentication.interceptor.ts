fca1e5891ebd1c277da05ce929e33f32a061a621
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2022-04-06 10:39:07 +0200

    Update authentication middleware

--- src/app/interceptors/authentication.interceptor.ts
+++ src/app/interceptors/authentication.interceptor.ts
@@ -20,36 +20,52 @@ const AUTH_SCHEME = 'Bearer';
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService,
-              private notificationService: NotificationService,
-              private router: Router) {
+  static readonly AUTH_ROUTES = [
+    {
+      allow: /^\/api(\/|$)/g,
+      blacklist: [
+        /^\/api\/ws\/websocket(\/|$)/g,
+        /^\/api\/roomsubscription(\/|$)/g,
+      ]
+    }
+  ];
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.authenticationService.isLoggedIn()) {
-      const token = this.authenticationService.getToken();
-      const cloned = req.clone({
-        headers: req.headers.set(AUTH_HEADER_KEY, `${AUTH_SCHEME} ${token}`)
-      });
-
-      return next.handle(cloned).pipe(tap({
-        next: (event: HttpEvent<any>) => {
-          if (event instanceof HttpResponse)  {
-            // Possible to do something with the response here
-          }
-        },
-        error: (err: any) => {
-          if (err instanceof HttpErrorResponse) {
-            // Catch 401 errors
-            if (err.status === 401) {
-              this.notificationService.show('You are not logged in.');
-              this.router.navigate(['home']);
-            }
-          }
-        }
-      }));
-    } else {
+    if (!this.authenticationService.isLoggedIn()) {
       return next.handle(req);
     }
+    const decodedUrl = req.url;
+    const needsAuthentication = AuthenticationInterceptor.AUTH_ROUTES.some(route =>
+      decodedUrl.match(route.allow) && !route.blacklist.some(entry => decodedUrl.match(entry)));
+    if (!needsAuthentication) {
+      return next.handle(req);
+    }
+
+    const token = this.authenticationService.getToken();
+    const cloned = req.clone({
+      headers: req.headers.set(AUTH_HEADER_KEY, `${AUTH_SCHEME} ${token}`)
+    });
+
+    return next.handle(cloned).pipe(tap({
+      next: (event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          // Possible to do something with the response here
+        }
+      },
+      error: (err: any) => {
+        if (err instanceof HttpErrorResponse && err.status === 401) {
+          this.authenticationService.logout();
+          this.notificationService.show('You are not logged in.');
+          this.router.navigate(['home']);
+        }
+      }
+    }));
   }
 }
