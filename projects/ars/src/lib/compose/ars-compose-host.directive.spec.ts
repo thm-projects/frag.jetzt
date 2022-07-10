92a7daaa9c15e5c1818949570fc1c3a5ee585afe
Lukas Haase <lukas.haase@mni.thm.de>
2022-07-10 20:51:38 +0200

    delete spec files

--- projects/ars/src/lib/compose/ars-compose-host.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { ArsComposeHostDirective } from './ars-compose-host.directive';
-//
-// describe('ArsComposeHostDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new ArsComposeHostDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
