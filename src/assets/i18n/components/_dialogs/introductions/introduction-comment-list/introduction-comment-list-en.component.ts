9b3b2fd2df6e98c435d0e54f38df3f79a1d12531
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2022-07-20 16:15:43 +0200

    Add sanitizer

--- src/assets/i18n/components/_dialogs/introductions/introduction-comment-list/introduction-comment-list-en.component.ts
+++ src/assets/i18n/components/_dialogs/introductions/introduction-comment-list/introduction-comment-list-en.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../../../../../app/services/util/session.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-introduction-comment-list-en',
@@ -10,8 +11,14 @@ export class IntroductionCommentListENComponent implements OnInit {
 
   constructor(
     public sessionInfo: SessionService,
+    private domSanitizer: DomSanitizer,
   ) {
   }
+
+  trust(url: string) {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
 
   ngOnInit(): void {
   }
