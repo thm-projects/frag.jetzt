ee2014b3abecc9f80975bfca39cf491cf796171f
Lukas Haase <lukas.haase@mni.thm.de>
2023-02-23 18:24:57 +0100

    add more options

--- src/app/models/livepoll-template.ts
+++ src/app/models/livepoll-template.ts
@@ -1,7 +1,17 @@
 export enum LivepollTemplate {
   Character,
   Symbol,
-  Agree
+  Agree,
+  Frequency,
+  YesNo,
+}
+
+export enum LivepollGroupKind {
+  Agreement,
+  Value,
+  Frequency,
+  Importance,
+  Misc
 }
 
 export interface LivepollNode<E extends LivepollTemplate> {
@@ -16,9 +26,17 @@ export interface LivepollNode<E extends LivepollTemplate> {
 
 export type LivepollTemplateContext = LivepollNode<LivepollTemplate>;
 
-type EachOf<E extends LivepollTemplate, T extends LivepollNode<E>> = { [E in T['kind']]: LivepollNode<E> };
+type EachOfTemplate<E extends LivepollTemplate, T extends LivepollNode<E>> = { [E in T['kind']]: LivepollNode<E> };
+type EachOfGroup<E extends LivepollGroupKind> = { [E in LivepollGroupKind]: LivepollTemplateContext[] };
 
-export const templateEntries: EachOf<LivepollTemplate, LivepollNode<LivepollTemplate>> = {
+export const templateEntries: EachOfTemplate<LivepollTemplate, LivepollNode<LivepollTemplate>> = {
+  [LivepollTemplate.Character]: {
+    kind: LivepollTemplate.Character,
+    isPlain: true,
+    name: 'character',
+    translate: false,
+    symbols: ['A', 'B', 'C', 'D']
+  },
   [LivepollTemplate.Symbol]: {
     kind: LivepollTemplate.Symbol,
     isPlain: false,
@@ -27,21 +45,42 @@ export const templateEntries: EachOf<LivepollTemplate, LivepollNode<LivepollTemp
     reverse: true,
     symbols: ['sentiment_very_dissatisfied', 'sentiment_neutral', 'sentiment_satisfied', 'sentiment_very_satisfied']
   },
-  [LivepollTemplate.Character]: {
-    kind: LivepollTemplate.Character,
-    isPlain: true,
-    name: 'character',
-    translate: false,
-    symbols: ['A', 'B', 'C', 'D']
-  },
   [LivepollTemplate.Agree]: {
     kind: LivepollTemplate.Agree,
     isPlain: true,
-    name: 'agree',
+    name: 'agree-multi',
     translate: true,
     reverse: true,
     length: 5
+  },
+  [LivepollTemplate.Frequency]: {
+    kind: LivepollTemplate.Frequency,
+    isPlain: true,
+    name: 'frequency-multi',
+    translate: true,
+    reverse: true,
+    length: 5
+  },
+  [LivepollTemplate.YesNo]: {
+    kind: LivepollTemplate.YesNo,
+    isPlain: false,
+    name: 'agree-binary',
+    translate: false,
+    reverse: true,
+    symbols: ['thumb_down', 'thumb_up']
   }
+};
+
+export const groupEntries: EachOfGroup<LivepollGroupKind> = {
+  [LivepollGroupKind.Frequency]: [
+    templateEntries[LivepollTemplate.Frequency]
+  ],
+  [LivepollGroupKind.Value]: [],
+  [LivepollGroupKind.Importance]: [],
+  [LivepollGroupKind.Agreement]: [
+    templateEntries[LivepollTemplate.Agree]
+  ],
+  [LivepollGroupKind.Misc]: []
 };
 
 export const templateContext: LivepollTemplateContext[] = Object.keys(templateEntries).map(entry => templateEntries[entry]);
