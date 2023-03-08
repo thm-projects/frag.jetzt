2c6cee0dccdaa6b95af7dd072fa2adf6feb97ede
Krayaty <fabian.schwickert@me.com>
2023-03-08 17:01:53 +0100

    Submenus for relevant option menu fields in chatroom component

--- projects/ars/src/lib/compose/elements/mat-sub-menu/mat-sub-menu.component.ts
+++ projects/ars/src/lib/compose/elements/mat-sub-menu/mat-sub-menu.component.ts
@@ -7,7 +7,7 @@ import {
 } from './ars-mat-sub-menu-config';
 
 @Component({
-  selector: 'app-mat-sub-menu',
+  selector: 'ars-mat-sub-menu',
   templateUrl: './mat-sub-menu.component.html',
   styleUrls: ['./mat-sub-menu.component.scss'],
 })
