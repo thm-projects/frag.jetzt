85871138a6bce21d6044b0f94caf704640bcc144
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2023-03-18 14:08:54 +0100

    Resolve "2nd collective ticket"

--- src/app/services/websockets/ws-comment.service.ts
+++ src/app/services/websockets/ws-comment.service.ts
@@ -2,23 +2,23 @@ import { Injectable } from '@angular/core';
 import { WsConnectorService } from './ws-connector.service';
 import { Observable } from 'rxjs';
 import { IMessage } from '@stomp/stompjs';
-
+import { UserRole } from 'app/models/user-roles.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WsCommentService {
+  constructor(private wsConnector: WsConnectorService) {}
 
-  constructor(
-    private wsConnector: WsConnectorService,
-    ) {
-  }
-
-  getCommentStream(roomId: string): Observable<IMessage> {
-    return this.wsConnector.getWatcher(`/topic/${roomId}.comment.stream`);
+  getCommentStream(roomId: string, role: UserRole): Observable<IMessage> {
+    return this.wsConnector.getWatcher(`/topic/${roomId}.comment.stream`, {
+      'ars-role': role,
+    });
   }
 
   getModeratorCommentStream(roomId: string): Observable<IMessage> {
-    return this.wsConnector.getWatcher(`/topic/${roomId}.comment.moderator.stream`);
+    return this.wsConnector.getWatcher(
+      `/topic/${roomId}.comment.moderator.stream`,
+    );
   }
 }
