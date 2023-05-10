3065bcc722999ad7862daa0f812f0df1acb56b98
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2023-05-11 00:58:07 +0200

    Fix firefox indexddb

--- src/app/services/persistence/dbroom-access.service.ts
+++ src/app/services/persistence/dbroom-access.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { NgxIndexedDBService } from 'ngx-indexed-db';
 import { UserRole } from '../../models/user-roles.enum';
+import { PersistentDataService } from '../util/persistent-data.service';
+import { tap } from 'rxjs';
 
 export interface SavedRoomAccess {
   userId: string;
@@ -10,24 +11,30 @@ export interface SavedRoomAccess {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DBRoomAccessService {
-
-  constructor(
-    private indexedDb: NgxIndexedDBService,
-  ) {
-  }
+  constructor(private persistentDataService: PersistentDataService) {}
 
   getAllByUser(userId: string) {
-    return this.indexedDb.getAllByIndex<SavedRoomAccess>('roomAccess', 'userId', IDBKeyRange.only(userId));
+    return this.persistentDataService.getAllByIndex<SavedRoomAccess>(
+      'roomAccess',
+      'userId',
+      IDBKeyRange.only(userId),
+    );
   }
 
   getByUserAndShortId(userId: string, shortId: string) {
-    return this.indexedDb.getByKey<SavedRoomAccess>('roomAccess', [userId, shortId]);
+    return this.persistentDataService.getByKey<SavedRoomAccess>('roomAccess', [
+      userId,
+      shortId,
+    ]);
   }
 
   updateEntry(access: SavedRoomAccess) {
-    return this.indexedDb.update<SavedRoomAccess>('roomAccess', access);
+    return this.persistentDataService.update<SavedRoomAccess>(
+      'roomAccess',
+      access,
+    );
   }
 }
---
+++ src/app/services/util/persistent-data.service.spec.ts
@@ -0,0 +1,17 @@
+/*import { TestBed } from '@angular/core/testing';
+
+import { PersistentDataService } from './persistent-data.service';
+
+describe('PersistentDataService', () => {
+  let service: PersistentDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PersistentDataService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+ */
