6261e7c7c1a93ef0adac8611331895a595ef21a0
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2023-06-08 13:30:00 +0200

    capacitor_setup solved; capacitor_add_plattform_android solved capacitor_compilation solved

---
+++ android/app/src/main/java/frag/jetzt/MainActivity.java
@@ -0,0 +1,5 @@
+package frag.jetzt;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'frag.jetzt',
+  appName: 'frag.jetzt',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
