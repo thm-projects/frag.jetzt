7dbf3e469bc2666ca86b6e9978b364ef84d3f47b
MarlinBrandstaedter <marlin.brandstaedter@mni.thm.de>
2023-06-26 16:44:13 +0200

    implemented new room tests

---
+++ cypress/e2e/create_room.feature
@@ -0,0 +1,17 @@
+Feature: Test functionalities for a room
+  This feature tests the basic functionalities of the room handling
+
+  Scenario: Create a room as a guest user
+    Given I am on the home page and skipped dialogues
+    Given I am logged in as a guest
+    When I click on the create room button
+    Then The room creation form should be visible
+    And I should be able to create a new room called "testRoom"
+    Then I should see the room options
+    And I should be able to save the options
+    Then I should see the room overview
+
+  Scenario: Cleanup
+    Given I am on the home page and skipped dialogues
+    Given that the test room will be deleted
+
---
+++ cypress/e2e/create_room/create_room.ts
@@ -0,0 +1,61 @@
+import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+export let roomData = {
+  shortId: "",
+  name: ""
+};
+
+When('I click on the create room button', () => {
+  cy.get('#create_session-button').click()
+});
+
+Then('The room creation form should be visible', () => {
+  cy.get('mat-dialog-container').find('app-room-create').should('be.visible')
+  cy.wait(300)
+});
+
+And('I should be able to create a new room called {string}', (testRoom) => {
+  //fill form
+  cy.get('[name="roomName"]').type(testRoom)
+  cy.get('mat-slide-toggle').click();
+
+  //intercept async request
+  cy.intercept('POST', '/api/room*').as('createRoomRequest')
+  cy.get('app-room-create').find('button.primary-confirm-button').click()
+
+  //save response body to global variable (roomData)
+  cy.wait('@createRoomRequest').then(({ request, response }) => {
+    expect(response.statusCode).to.eq(200)
+    expect(response.body).to.be.not.empty
+    roomData = response.body
+  })
+});
+
+Then('I should see the room options', () => {
+cy.get('mat-dialog-container').find('app-room-settings-overview').should('be.visible')
+
+});
+
+And('I should be able to save the options', () => {
+  cy.get('mat-dialog-container').find('app-room-settings-overview')
+  cy.get('mat-dialog-container').find('.primary-confirm-button').click()
+  cy.wait(300)
+});
+
+Then('I should see the room overview', () => {
+  //test if redirect worked by checking the url
+  cy.url().should('include', '/creator/room/' + roomData.shortId)
+  //check page content by examining the room title
+  cy.get('app-room-creator-page').find('mat-card-title').should('contain.text', roomData.name)
+});
+
+Given('that the test room will be deleted', () => {
+    cy.visit('/creator/room/' + roomData.shortId);
+    cy.wait(500)
+    cy.get('#session-button').click();
+    cy.get('.mat-menu-content').contains('Raum löschen').click()
+    cy.get('app-room-delete').find('button.alert-confirm-button').click()
+    cy.wait(500)
+    //check if redirect to /user page worked after deletion
+    cy.url().should('include', '/user')
+});
