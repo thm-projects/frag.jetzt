fece11601a30cb9103cdd3c268ed9ed6a57b91fe
lasse <lasse.kreuter@mni.thm.de>
2023-06-27 20:09:43 +0200

    Reset to working splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,24 @@ const config: CapacitorConfig = {
   appId: 'frag.jetzt',
   appName: 'frag.jetzt',
   webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 300,
+      launchAutoHide: true,
+      launchFadeOutDuration: 5000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
   server: {
     androidScheme: 'https',
     hostname: "frag.jetzt"
