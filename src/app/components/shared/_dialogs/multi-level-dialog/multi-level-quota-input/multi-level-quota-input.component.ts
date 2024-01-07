3737dfba28fb5f60a16db48ca48d1b1b66c3b2c7
anas <anas.ahakki@mni.thm.de>
2024-01-08 00:11:25 +0100

    Make and adjust the limt of all the input quotas

--- src/app/components/shared/_dialogs/multi-level-dialog/multi-level-quota-input/multi-level-quota-input.component.ts
+++ src/app/components/shared/_dialogs/multi-level-dialog/multi-level-quota-input/multi-level-quota-input.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { BuiltAction, DYNAMIC_INPUT, QuotaInputAction, TextInputAction } from '../interface/multi-level-dialog.types';
+import {
+  BuiltAction,
+  DYNAMIC_INPUT,
+  QuotaInputAction,
+  TextInputAction,
+} from '../interface/multi-level-dialog.types';
 
 @Component({
   selector: 'app-multi-level-quota-input',
   templateUrl: './multi-level-quota-input.component.html',
-  styleUrls: ['./multi-level-quota-input.component.scss']
+  styleUrls: ['./multi-level-quota-input.component.scss'],
 })
 export class MultiLevelQuotaInputComponent implements OnInit {
   data = inject(DYNAMIC_INPUT) as BuiltAction<QuotaInputAction>;
   hidden = Boolean(this.data.hidden);
   readonly initialHidden = this.hidden;
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  clampValue(value: number, min: number, max: number): number {
+    return Math.min(Math.max(value, min), max);
   }
-
 }
