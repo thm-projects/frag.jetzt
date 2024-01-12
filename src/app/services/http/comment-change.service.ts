ecad572d0272129124b37808026a1a551d1eba33
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-01-12 09:56:47 +0100

    Finish frontend

--- src/app/services/http/comment-change.service.ts
+++ src/app/services/http/comment-change.service.ts
@@ -7,107 +7,49 @@ import { CommentChange } from '../../models/comment-change';
 
 const httpOptions = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
-export interface CommentChangeSubscription {
-  id: string;
-  commentId: string;
-  roomId: string;
-  accountId: string;
-}
-
-export interface RoomCommentChangeSubscription {
-  id: string;
-  roomId: string;
-  accountId: string;
-}
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentChangeService extends BaseHttpService {
-
   private apiUrl = {
     base: '/api',
     commentChange: '/comment-change',
     find: '/find',
-    commentSubscribe: '/comment-subscribe',
-    roomSubscribe: '/room-subscribe'
   };
 
-  constructor(
-    private http: HttpClient,
-  ) {
+  constructor(private http: HttpClient) {
     super();
   }
 
   findAllChangesSince(date: Date): Observable<CommentChange[]> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.find;
-    return this.http.post<CommentChange[]>(connectionUrl, {
-      properties: {},
-      externalFilters: {
-        lastAction: date.getTime()
-      }
-    }, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<CommentChange[]>('findAllChangesSince')),
-    );
+    const connectionUrl =
+      this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.find;
+    return this.http
+      .post<CommentChange[]>(
+        connectionUrl,
+        {
+          properties: {},
+          externalFilters: {
+            lastAction: date.getTime(),
+          },
+        },
+        httpOptions,
+      )
+      .pipe(
+        tap((_) => ''),
+        catchError(this.handleError<CommentChange[]>('findAllChangesSince')),
+      );
   }
 
   getCommentChangeById(id: string): Observable<CommentChange> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + '/' + id;
+    const connectionUrl =
+      this.apiUrl.base + this.apiUrl.commentChange + '/' + id;
     return this.http.get<CommentChange>(connectionUrl, httpOptions).pipe(
-      tap(_ => ''),
+      tap((_) => ''),
       catchError(this.handleError<CommentChange>('getCommentChangeById')),
-    );
-  }
-
-  createCommentSubscription(commentId: string): Observable<CommentChangeSubscription> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.commentSubscribe + '/' + commentId;
-    return this.http.post<CommentChangeSubscription>(connectionUrl, null, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<CommentChangeSubscription>('createCommentSubscription')),
-    );
-  }
-
-  deleteCommentSubscription(commentId: string): Observable<any> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.commentSubscribe + '/' + commentId;
-    return this.http.delete<any>(connectionUrl, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<any>('deleteCommentSubscription')),
-    );
-  }
-
-  getCommentSubscriptions(): Observable<CommentChangeSubscription[]> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.commentSubscribe;
-    return this.http.get<CommentChangeSubscription[]>(connectionUrl, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<CommentChangeSubscription[]>('getCommentSubscriptions')),
-    );
-  }
-
-  createRoomSubscription(roomId: string): Observable<RoomCommentChangeSubscription> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.roomSubscribe + '/' + roomId;
-    return this.http.post<RoomCommentChangeSubscription>(connectionUrl, null, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<RoomCommentChangeSubscription>('createRoomSubscription')),
-    );
-  }
-
-  deleteRoomSubscription(roomId: string): Observable<any> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.roomSubscribe + '/' + roomId;
-    return this.http.delete<any>(connectionUrl, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<any>('deleteRoomSubscription')),
-    );
-  }
-
-  getRoomSubscriptions(): Observable<RoomCommentChangeSubscription[]> {
-    const connectionUrl = this.apiUrl.base + this.apiUrl.commentChange + this.apiUrl.roomSubscribe;
-    return this.http.get<RoomCommentChangeSubscription[]>(connectionUrl, httpOptions).pipe(
-      tap(_ => ''),
-      catchError(this.handleError<RoomCommentChangeSubscription[]>('getRoomSubscriptions')),
     );
   }
 }
---
+++ src/app/services/http/web-push.service.spec.ts
@@ -0,0 +1,17 @@
+/*import { TestBed } from '@angular/core/testing';
+
+import { WebPushService } from './web-push.service';
+
+describe('WebPushService', () => {
+  let service: WebPushService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(WebPushService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+ */
