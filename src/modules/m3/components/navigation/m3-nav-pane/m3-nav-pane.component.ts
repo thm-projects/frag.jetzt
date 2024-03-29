f547298b429addd3c60109eaf764806fb3926ab7
lphs11 <lukas.haase@mni.thm.de>
2024-03-29 17:55:31 +0100

    add animation

--- src/modules/m3/components/navigation/m3-nav-pane/m3-nav-pane.component.ts
+++ src/modules/m3/components/navigation/m3-nav-pane/m3-nav-pane.component.ts
@@ -3,7 +3,7 @@ import { M3NavRailComponent } from '../m3-nav-rail/m3-nav-rail.component';
 import { M3NavDrawerComponent } from '../m3-nav-drawer/m3-nav-drawer.component';
 import { MatFabButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { NgIf, NgStyle, NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgIf, NgStyle, NgTemplateOutlet } from '@angular/common';
 import { M3LabelButton } from '../../buttons/label-button';
 import { M3Badge, M3Icon, M3Label } from '../../buttons/base';
 import { MatList, MatListItem } from '@angular/material/list';
@@ -26,6 +26,7 @@ import { MatList, MatListItem } from '@angular/material/list';
     M3Badge,
     MatList,
     MatListItem,
+    NgClass,
   ],
   templateUrl: './m3-nav-pane.component.html',
   styleUrl: './m3-nav-pane.component.scss',
