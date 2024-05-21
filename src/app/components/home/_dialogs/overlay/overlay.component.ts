8082d251f16ee496ecd87529419aafff55da86a8
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-05-21 20:30:42 +0200

    Add configuration and update theming

--- src/app/components/home/_dialogs/overlay/overlay.component.ts
+++ src/app/components/home/_dialogs/overlay/overlay.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { DialogConfirmActionButtonType } from '../../../shared/dialog/dialog-action-buttons/dialog-action-buttons.component';
-import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-overlay',
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.scss'],
 })
-export class OverlayComponent {
-  confirmButtonType: DialogConfirmActionButtonType;
-
-  constructor(private dialogRef: MatDialogRef<OverlayComponent>) {
-    this.confirmButtonType = DialogConfirmActionButtonType.Primary;
-  }
-
-  onResume() {
-    this.dialogRef.close(true);
-  }
-
-  onAbort() {
-    this.dialogRef.close(false);
-  }
-}
+export class OverlayComponent {}
