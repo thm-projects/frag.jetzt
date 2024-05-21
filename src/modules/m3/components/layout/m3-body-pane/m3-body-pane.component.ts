f72cfea52b04c6f917b66be2b99f6b1fcc71c6d8
lphs11 <lukas.haase@mni.thm.de>
2024-05-21 09:46:35 +0200

    remove unneeded class, add body pane property for mobile centering

--- src/modules/m3/components/layout/m3-body-pane/m3-body-pane.component.ts
+++ src/modules/m3/components/layout/m3-body-pane/m3-body-pane.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'm3-body-pane',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   imports: [],
   template: '<ng-content></ng-content>',
   styleUrl: './m3-body-pane.component.scss',
+  host: {
+    '[class.m3-body-pane-center-single-pane]': 'centerSinglePane',
+  },
 })
-export class M3BodyPaneComponent {}
+export class M3BodyPaneComponent {
+  @Input('center-single-pane') centerSinglePane!: boolean;
+}
