71b1d50599348b102e42f74bbd87004675431af2
lphs11 <lukas.haase@mni.thm.de>
2024-05-29 13:44:47 +0200

    refactor question wall

--- src/app/components/shared/questionwall/question-wall/support-components/question-wall-comment-focus/question-wall-comment-focus.component.ts
+++ src/app/components/shared/questionwall/question-wall/support-components/question-wall-comment-focus/question-wall-comment-focus.component.ts
@@ -5,11 +5,12 @@ import { ArsModule } from '../../../../../../../../projects/ars/src/lib/ars.modu
 import { ArsDateFormatter } from '../../../../../../../../projects/ars/src/lib/services/ars-date-formatter.service';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { CustomMarkdownModule } from '../../../../../../base/custom-markdown/custom-markdown.module';
 
 @Component({
   selector: 'app-question-wall-comment-focus',
   standalone: true,
-  imports: [ArsModule, MatButton, MatIcon],
+  imports: [ArsModule, MatButton, MatIcon, CustomMarkdownModule],
   templateUrl: './question-wall-comment-focus.component.html',
   styleUrl: './question-wall-comment-focus.component.scss',
 })
