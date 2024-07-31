91a5454f9fe04865255c8c3a0808d0382e98af4d
lphs11 <lukas.haase@mni.thm.de>
2024-07-31 20:22:29 +0200

    add default placeholder, remove todos in m3-typescale

---
+++ src/app/components/shared/questionwall/question-wall/support-components/qw-default-placeholder/qw-default-placeholder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'qw-default-placeholder',
+  standalone: true,
+  imports: [],
+  templateUrl: './qw-default-placeholder.component.html',
+  styleUrl: './qw-default-placeholder.component.scss',
+})
+export class QwDefaultPlaceholderComponent {}
