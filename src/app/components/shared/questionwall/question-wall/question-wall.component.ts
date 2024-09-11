731869c428525ed9506b396a70241f7e3f52dd13
lphs11 <lukas.haase@mni.thm.de>
2024-09-11 22:29:24 +0200

    remove unused

--- src/app/components/shared/questionwall/question-wall/question-wall.component.ts
+++ src/app/components/shared/questionwall/question-wall/question-wall.component.ts
@@ -10,7 +10,6 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 import { CommentService } from '../../../../services/http/comment.service';
-import { Comment } from '../../../../models/comment';
 import { WsCommentService } from '../../../../services/websockets/ws-comment.service';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
@@ -20,7 +19,6 @@ import { RoomDataService } from '../../../../services/util/room-data.service';
 import { User } from '../../../../models/user';
 import { SessionService } from '../../../../services/util/session.service';
 import { Room } from '../../../../models/room';
-import { IntroductionQuestionWallComponent } from '../../_dialogs/introductions/introduction-question-wall/introduction-question-wall.component';
 import {
   BrainstormingFilter,
   Period,
@@ -45,6 +43,7 @@ import { FilteredDataAccess } from '../../../../utils/filtered-data-access';
 import { createComponentBuilder } from '../component-builder-support';
 import i18nRaw from './translation/qw.i18n.json';
 import { I18nLoader } from '../../../../base/i18n/i18n-loader';
+
 const i18n = I18nLoader.load(i18nRaw);
 
 interface CommentCache {
@@ -163,44 +162,9 @@ export class QuestionWallComponent implements OnInit, AfterViewInit, OnDestroy {
     this.initNavigation();
   }
 
-  public wrap<E>(e: E, action: (e: E) => void) {
-    action(e);
-  }
-
-  public notUndefined<E>(e: E, action: (e: E) => void, elsePart?: () => void) {
-    if (e) {
-      action(e);
-    } else if (elsePart) {
-      elsePart();
-    }
-  }
-
   handlePageEvent(e: PageEvent) {
     this.pageIndex = e.pageIndex;
     this.pageSize = e.pageSize;
-  }
-
-  getSlicedComments(): ForumComment[] {
-    const start = this.pageIndex * this.pageSize;
-    const end = start + this.pageSize;
-    return this.comments.slice(start, end);
-  }
-
-  toggleSideList() {
-    // this.sidelistExpanded = !this.sidelistExpanded;
-    // if (this.sidelistExpanded) {
-    //   this.sidelist.setPx(450);
-    //   setTimeout(() => {
-    //     this.headerComponent.setPx(50);
-    //     this.footerComponent.setPx(50);
-    //   }, 200);
-    // } else {
-    //   this.sidelist.setPx(0);
-    //   setTimeout(() => {
-    //     this.headerComponent.setPx(0);
-    //     this.footerComponent.setPx(0);
-    //   }, 200);
-    // }
   }
 
   getURL() {
@@ -212,7 +176,6 @@ export class QuestionWallComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.initKeySupport();
     this.session.onInit.subscribe(() => {
       this.cdr.detectChanges();
     });
@@ -222,35 +185,8 @@ export class QuestionWallComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  initKeySupport() {
-    this.wrap(this.keySupport, (key) => {
-      key.addKeyEvent('ArrowRight', () => this.nextComment());
-      key.addKeyEvent('ArrowLeft', () => this.prevComment());
-      key.addKeyEvent('ArrowDown', () => this.nextComment());
-      key.addKeyEvent('ArrowUp', () => this.prevComment());
-      key.addKeyEvent('l', () => this.toggleSideList());
-      key.addKeyEvent('q', () => (this.qrCodeExpanded = !this.qrCodeExpanded));
-    });
-  }
-
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit');
-    // document.getElementById('header_rescale').style.display = 'none';
-    // document.getElementById('footer_rescale').style.display = 'none';
-    // setTimeout(() => {
-    //   Rescale.requestFullscreen();
-    // }, 0);
-    // setTimeout(() => {
-    //   Array.from(
-    //     document
-    //       .getElementsByClassName('questionwall-screen')[0]
-    //       .getElementsByTagName('button'),
-    //   ).forEach((e) => {
-    //     e.addEventListener('keydown', (e1) => {
-    //       e1.preventDefault();
-    //     });
-    //   });
-    // }, 100);
   }
 
   ngOnDestroy(): void {
@@ -277,240 +213,9 @@ export class QuestionWallComponent implements OnInit, AfterViewInit, OnDestroy {
     Rescale.exitFullscreen();
   }
 
-  isOld(commentId: string): boolean {
-    return this.commentCache[commentId].old;
-  }
-
-  nextComment() {
-    this.moveComment(1);
-  }
-
-  prevComment() {
-    this.moveComment(-1);
-  }
-
-  getDOMComments() {
-    return Array.from(
-      document.getElementsByClassName('questionwall-comment-anchor'),
-    );
-  }
-
-  getDOMCommentFocus() {
-    return this.getDOMComments()[this.getCommentFocusIndex()];
-  }
-
-  getCommentFocusIndex() {
-    return this.comments.findIndex((c) => c.id === this.commentFocusId);
-  }
-
-  moveComment(fx: number) {
-    if (!this.comments.length) {
-      return;
-    }
-    if (!this.commentFocusId) {
-      this.focusComment(this.comments[0]);
-      return;
-    }
-    const cursor = this.getCommentFocusIndex();
-    if (cursor + fx >= this.comments.length || cursor + fx < 0) {
-      return;
-    } else {
-      this.focusComment(this.comments[cursor + fx]);
-    }
-  }
-
-  focusComment(comment: ForumComment) {
-    this.firstPassIntroduction = false;
-    if (this.commentFocusId === comment.id) {
-      return;
-    }
-    this.commentFocusId = null;
-    this.commentFocus = null;
-    setTimeout(() => {
-      this.commentFocusId = comment.id;
-      this.commentFocus = comment;
-      const entry = this.commentCache[comment.id];
-      if (!entry.old) {
-        entry.old = true;
-        this.unreadComments--;
-      }
-      // this.getDOMCommentFocus().scrollIntoView({
-      //   behavior: 'smooth',
-      //   block: 'center',
-      // });
-    }, 1);
-  }
-
-  toggleFocusIncomingComments() {
-    this.focusIncomingComments = !this.focusIncomingComments;
-  }
-
-  applyUserMap(user: string) {
-    this.userSelection = false;
-    this.self.session.filter.filterUserByNumber(user);
-  }
-
-  openUserMap() {
-    if (this.userSelection) {
-      return;
-    }
-    this.userSelection = true;
-  }
-
-  cancelUserMap() {
-    this.userSelection = false;
-    this.self.session.filter.deactivateFilter();
-  }
-
-  // leave() {
-  //   this.router.navigate([
-  //     '/' +
-  //       this.resolveUserRole() +
-  //       '/room/' +
-  //       this.sessionService.currentRoom.shortId +
-  //       '/comments',
-  //   ]);
-
-  // }
-
-  likeComment(comment: Comment) {
-    this.commentService.voteUp(comment, this.user.id).subscribe();
-  }
-
-  dislikeComment(comment: Comment) {
-    this.commentService.voteDown(comment, this.user.id).subscribe();
-  }
-
-  openConversation(comment: Comment) {
-    sessionStorage.setItem(
-      'conversation-fallback-url',
-      decodeURI(this.router.url),
-    );
-    this.router.navigate([
-      '/' +
-        this.resolveUserRole() +
-        '/room/' +
-        this.sessionService.currentRoom.shortId +
-        '/comment/' +
-        comment.id +
-        '/conversation',
-    ]);
-  }
-
-  sliderChange(evt: { value: number }) {
-    this.fontSize = evt.value;
-  }
-
-  openSiteIntroduction() {
-    this.dialog.open(IntroductionQuestionWallComponent, {
-      autoFocus: false,
-    });
-  }
-
-  onUpdateFiltering() {
-    this.animationTrigger = false;
-    // setTimeout(() => {
-    //   this.animationTrigger = true;
-    //   if (
-    //     this.comments.length < 1 ||
-    //     !this.comments.some((c) => c.id === this.commentFocusId)
-    //   ) {
-    //     this.commentFocusId = null;
-    //   }
-    // });
-  }
-
-  getCommentIcon(comment: Comment): string {
-    const isFromOwner = this.room.ownerId === comment?.creatorId;
-    let isFromModerator = false;
-    this.sessionService
-      .getModeratorsOnce()
-      .subscribe(
-        (mods) =>
-          (isFromModerator = mods.some(
-            (mod) => mod.accountId === comment?.creatorId,
-          )),
-      );
-    if ((comment?.brainstormingSessionId || null) !== null) {
-      return ' tips_and_updates';
-    } else if (isFromOwner) {
-      return 'co_present';
-    } else if (isFromModerator) {
-      return 'support_agent';
-    }
-    return '';
-  }
-
-  getCommentIconClass(comment: Comment): string {
-    const isFromOwner = this.room.ownerId === comment?.creatorId;
-    let isFromModerator = false;
-    this.sessionService
-      .getModeratorsOnce()
-      .subscribe(
-        (mods) =>
-          (isFromModerator = mods.some(
-            (mod) => mod.accountId === comment?.creatorId,
-          )),
-      );
-    if (
-      (comment?.brainstormingSessionId || null) !== null ||
-      isFromOwner ||
-      isFromModerator
-    ) {
-      return '';
-    }
-    return 'material-icons-outlined';
-  }
-
-  generateCommentNumber(comment: Comment): string {
-    if (!comment?.number) {
-      return '';
-    }
-    const meta = comment.number.split('/');
-    const topLevelNumber = meta[0];
-    const number = meta[meta.length - 1];
-    let message = '';
-    if (meta.length === 1) {
-      this.translateService
-        .get('comment-list.question-number', { number })
-        .subscribe((msg) => (message = msg.split('/')));
-      return message;
-    }
-    this.translateService
-      .get('comment-list.comment-number', {
-        topLevelNumber,
-        number,
-        level: meta.length - 1,
-      })
-      .subscribe((msg) => (message = msg.split('/')));
-    return message;
-  }
-
-  private resolveUserRole(): string {
-    return this.roomState.getCurrentRole()?.toLowerCase?.() || 'participant';
-  }
-
-  private refreshUserMap() {
-    this.userMap.clear();
-    this.comments.forEach((comment) => {
-      this.userMap.set(
-        comment.creatorId,
-        (this.userMap.get(comment.creatorId) || 0) + 1,
-      );
-    });
-    this.userList.length = 0;
-    this.userMap.forEach((num, user) => {
-      this.userList.push([num, user]);
-    });
-  }
-
   private initNavigation() {
     applyRoomNavigation(this.injector)
       .pipe(takeUntil(this.destroyer))
       .subscribe();
-  }
-
-  debugComments() {
-    console.log(this.session, this.session?.comments);
   }
 }
