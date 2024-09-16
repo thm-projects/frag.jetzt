30468037294083ad4a37eb892ce22c738e0e598e
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-16 13:18:07 +0200

    1491-Add missing Close button

--- src/app/components/home/home-page/feature-grid/feature-grid-dialog/feature-grid-dialog.component.ts
+++ src/app/components/home/home-page/feature-grid/feature-grid-dialog/feature-grid-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from "@angular/material/dialog";
 import { FeatureGridComponent } from '../feature-grid.component';
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-feature-grid-dialog',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, FeatureGridComponent],
+  imports: [MatDialogTitle, MatDialogContent, FeatureGridComponent, MatDialogActions, MatDialogClose, MatButton],
   templateUrl: './feature-grid-dialog.component.html',
   styleUrl: './feature-grid-dialog.component.scss',
 })
