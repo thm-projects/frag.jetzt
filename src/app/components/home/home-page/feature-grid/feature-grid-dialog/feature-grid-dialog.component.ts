4470ec56ef53a7077873a22809000ffa966fc8bb
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-09-17 11:31:53 +0200

    Add i18n support

--- src/app/components/home/home-page/feature-grid/feature-grid-dialog/feature-grid-dialog.component.ts
+++ src/app/components/home/home-page/feature-grid/feature-grid-dialog/feature-grid-dialog.component.ts
@@ -1,13 +1,30 @@
+import rawI18n from './i18n.json';
+import { I18nLoader } from 'app/base/i18n/i18n-loader';
+const i18n = I18nLoader.load(rawI18n);
 import { Component } from '@angular/core';
-import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from "@angular/material/dialog";
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 import { FeatureGridComponent } from '../feature-grid.component';
-import { MatButton } from "@angular/material/button";
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-feature-grid-dialog',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, FeatureGridComponent, MatDialogActions, MatDialogClose, MatButton],
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    FeatureGridComponent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButton,
+  ],
   templateUrl: './feature-grid-dialog.component.html',
   styleUrl: './feature-grid-dialog.component.scss',
 })
-export class FeatureGridDialogComponent {}
+export class FeatureGridDialogComponent {
+  protected readonly i18n = i18n;
+}
