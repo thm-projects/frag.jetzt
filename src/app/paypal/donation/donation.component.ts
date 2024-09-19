c02145699e0dce9627f2bf6dc548f88be3dea8e1
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-19 16:22:39 +0200

    edit donation page

--- src/app/paypal/donation/donation.component.ts
+++ src/app/paypal/donation/donation.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { M3BodyPaneComponent } from 'modules/m3/components/layout/m3-body-pane/m3-body-pane.component';
 import { M3SupportingPaneComponent } from 'modules/m3/components/layout/m3-supporting-pane/m3-supporting-pane.component';
+import { MatCard, MatCardContent, MatCardHeader } from "@angular/material/card";
+import { MatFormField } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { MatInput } from "@angular/material/input";
 
 @Component({
   selector: 'app-donation',
   standalone: true,
-  imports: [M3BodyPaneComponent, M3SupportingPaneComponent],
+  imports: [M3BodyPaneComponent, M3SupportingPaneComponent, MatCard, MatCardHeader, MatCardContent, MatFormField, FormsModule, MatInput],
   templateUrl: './donation.component.html',
   styleUrl: './donation.component.scss',
 })
