543e247804f54c5a9678914d73d66533a4bc4146
Phi-Long Tran <phi.long.tran@mni.thm.de>
2024-09-19 19:22:26 +0200

    Created new component

---
+++ src/app/paypal/payment/payment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment.component.html',
+  styleUrl: './payment.component.scss'
+})
+export class PaymentComponent {
+
+}
