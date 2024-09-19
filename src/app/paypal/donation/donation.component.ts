c55c84e2cd3f2728783b48ad0bfc7c744f27fe7e
MackAlexander <alexander.mack@mni.thm.de>
2024-09-19 15:45:24 +0200

    add component

--- src/app/paypal/donation/donation.component.ts
+++ src/app/paypal/donation/donation.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { M3BodyPaneComponent } from 'modules/m3/components/layout/m3-body-pane/m3-body-pane.component';
+import { M3SupportingPaneComponent } from 'modules/m3/components/layout/m3-supporting-pane/m3-supporting-pane.component';
 
 @Component({
   selector: 'app-donation',
   standalone: true,
-  imports: [],
+  imports: [M3BodyPaneComponent, M3SupportingPaneComponent],
   templateUrl: './donation.component.html',
   styleUrl: './donation.component.scss',
 })
