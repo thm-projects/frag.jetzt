61b65d962813f13d6e1c2a288c308ea7ec888fcf
anna76 <git.thm.de/anna76@gitlab.com>
2024-09-19 20:29:16 +0200

    Generate and display QR code

--- src/app/components/home/_dialogs/cookies/cookies.component.ts
+++ src/app/components/home/_dialogs/cookies/cookies.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataProtectionComponent } from '../data-protection/data-protection.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { language } from 'app/base/language/language';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-cookies',
   templateUrl: './cookies.component.html',
   styleUrls: ['./cookies.component.scss'],
 })
-export class CookiesComponent {
+export class CookiesComponent implements OnInit {
   protected readonly lang = language;
+  isMobile: boolean = false;
 
   constructor(
     private dialog: MatDialog,
     private dialogRef: MatDialogRef<CookiesComponent>,
+    private breakpointObserver: BreakpointObserver,
   ) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches; // This will be true if the device is a phone
+      });
+  }
 
   acceptCookies() {
     this.dialogRef.close(true);
