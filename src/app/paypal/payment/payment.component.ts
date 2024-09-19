3a78ab2472e9e023f0486260ac490ebb3edc5073
Phi-Long Tran <phi.long.tran@mni.thm.de>
2024-09-19 19:47:58 +0200

    Create routing path

--- src/app/paypal/payment/payment.component.ts
+++ src/app/paypal/payment/payment.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { M3BodyPaneComponent } from 'modules/m3/components/layout/m3-body-pane/m3-body-pane.component';
+import { M3SupportingPaneComponent } from 'modules/m3/components/layout/m3-supporting-pane/m3-supporting-pane.component';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [M3BodyPaneComponent, M3SupportingPaneComponent],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss'
 })
