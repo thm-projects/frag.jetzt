5481688e7e1324b0402f2e8c8acf80d45ed700db
MackAlexander <alexander.mack@mni.thm.de>
2024-09-19 15:29:25 +0200

    Create routing path

---
+++ src/app/paypal/donation/donation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation',
+  standalone: true,
+  imports: [],
+  templateUrl: './donation.component.html',
+  styleUrl: './donation.component.scss',
+})
+export class DonationComponent {}
