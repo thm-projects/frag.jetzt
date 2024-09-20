82e108e582fa24ecab8199a59dabcd231a164c16
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-09-20 14:14:07 +0200

    Organize code

--- src/app/components/home/_dialogs/cookies/cookies.component.ts
+++ src/app/components/home/_dialogs/cookies/cookies.component.ts
@@ -1,60 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  OnDestroy,
+  OnInit,
+  Signal,
+  signal,
+} from '@angular/core';
 import { DataProtectionComponent } from '../data-protection/data-protection.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { language } from 'app/base/language/language';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { windowWatcher } from 'modules/navigation/utils/window-watcher';
+import { M3WindowSizeClass } from 'modules/m3/components/navigation/m3-navigation-types';
 import { isDark } from '../../../../base/theme/theme';
+
+interface BadgeData {
+  alt: string;
+  src: string;
+}
+
+type ShowBadgeType = 'appstore' | 'playstore' | 'both' | 'none';
 
 @Component({
   selector: 'app-cookies',
   templateUrl: './cookies.component.html',
   styleUrls: ['./cookies.component.scss'],
 })
-export class CookiesComponent implements OnInit {
+export class CookiesComponent implements OnInit, OnDestroy {
   protected readonly lang = language;
-  isMobile: boolean = false;
-  isAndroid: boolean = false;
-  isIOS: boolean = false;
-  isPWA: boolean = window.matchMedia('(display-mode: standalone)').matches;
+  protected readonly isMobile = signal(false);
+  protected readonly badgeType: Signal<ShowBadgeType> = computed(() => {
+    const isDesktop = windowWatcher.windowState() !== M3WindowSizeClass.Compact;
+    if (isDesktop) {
+      return 'both';
+    }
+    const isInstalled = this.isPwa();
+    if (isInstalled) {
+      return 'none';
+    }
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isAndroid = /Android/i.test(navigator.userAgent);
+    if (isIOS) {
+      return 'appstore';
+    }
+    return isAndroid ? 'playstore' : 'both';
+  });
+  protected readonly googleUrl =
+    'https://play.google.com/store/apps/details?id=com.uninow.thm&hl=de';
+  protected readonly appleUrl =
+    'https://apps.apple.com/us/app/thm-app/id1644060104?itscg=30200&itsct=apps_box_badge&mttnsubad=1644060104';
+  private isPwa = signal(false);
+  private readonly pwaMatcher = window.matchMedia('(display-mode: standalone)');
+  private pwaListener = null;
 
   constructor(
     private dialog: MatDialog,
     private dialogRef: MatDialogRef<CookiesComponent>,
-    private breakpointObserver: BreakpointObserver,
   ) {}
 
   ngOnInit(): void {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset])
-      .subscribe((result) => {
-        this.isMobile = result.matches; // This will be true if the device is a phone
-      });
-
-    this.isAndroid = /Android/i.test(navigator.userAgent);
-    this.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    this.isPwa.set(this.pwaMatcher.matches);
+    this.pwaListener = this.pwaMatcher.addEventListener('change', (e) => {
+      this.isPwa.set(e.matches);
+    });
   }
 
-  getAppStoreBadge(language: string): string {
-    if (language === 'de') {
-      if (isDark()) {
-        return 'assets/images/appstore_de_white.svg';
-      } else {
-        return 'assets/images/appstore_de_black.svg';
-      }
-    } else if (language === 'en') {
-      if (isDark()) {
-        return 'assets/images/appstore_en_white.svg';
-      } else {
-        return 'assets/images/appstore_en_black.svg';
-      }
-    } else if (language === 'fr') {
-      if (isDark()) {
-        return 'assets/images/appstore_fr_white.svg';
-      } else {
-        return 'assets/images/appstore_fr_black.svg';
-      }
+  ngOnDestroy(): void {
+    this.pwaMatcher.removeEventListener('change', this.pwaListener);
+  }
+
+  getAppStoreBadge(badgeType: ShowBadgeType): BadgeData {
+    switch (language()) {
+      case 'de':
+        return {
+          alt: 'Lade die App im App Store herunter',
+          src:
+            isDark() && badgeType === 'appstore'
+              ? 'assets/images/appstore_de_white.svg'
+              : 'assets/images/appstore_de_black.svg',
+        };
+      case 'fr':
+        return {
+          alt: "Télécharge l'application dans l'App Store",
+          src:
+            isDark() && badgeType === 'appstore'
+              ? 'assets/images/appstore_fr_white.svg'
+              : 'assets/images/appstore_fr_black.svg',
+        };
+      default:
+        return {
+          alt: 'Download on the App Store',
+          src:
+            isDark() && badgeType === 'appstore'
+              ? 'assets/images/appstore_en_white.svg'
+              : 'assets/images/appstore_en_black.svg',
+        };
     }
-    return 'assets/images/appstore_de_black.svg';
+  }
+
+  getPlayStoreBadge(): BadgeData {
+    switch (language()) {
+      case 'de':
+        return {
+          alt: 'Lade die App im Play Store herunter',
+          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_German.png',
+        };
+      case 'fr':
+        return {
+          alt: "Télécharge l'application dans l'Play Store",
+          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_French-CA.png',
+        };
+      default:
+        return {
+          alt: 'Get it on Google Play',
+          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_English.png',
+        };
+    }
   }
 
   acceptCookies() {
