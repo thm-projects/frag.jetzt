7ccb559a46422010a1b8b9e76308f06603672fa5
DuniaHamad <dunia.hamad@mni.thm.de>
2024-09-23 12:08:24 +0200

    feat: update donation message for more motivational and engaging tone

--- src/app/paypal/donation/donation.component.ts
+++ src/app/paypal/donation/donation.component.ts
@@ -6,6 +6,7 @@ import { CustomMarkdownModule } from 'app/base/custom-markdown/custom-markdown.m
 import { ContextPipe } from 'app/base/i18n/context.pipe';
 import rawI18n from './i18n.json';
 import { I18nLoader } from 'app/base/i18n/i18n-loader';
+import { MatIcon } from '@angular/material/icon';
 const i18n = I18nLoader.load(rawI18n);
 
 @Component({
@@ -17,12 +18,13 @@ const i18n = I18nLoader.load(rawI18n);
     MatButtonModule,
     CustomMarkdownModule,
     ContextPipe,
+    MatIcon,
   ],
   templateUrl: './donation.component.html',
   styleUrl: './donation.component.scss',
 })
 export class DonationComponent {
   protected readonly i18n = i18n;
-  url = 'https://www.sandbox.paypal.com/ncp/payment/J4AV5JU5RMLU6'; // TODO from sandbox to regular PayPal account
+  url = 'https://www.sandbox.paypal.com/ncp/payment/J4AV5JU5RMLU6';
   qrCodeSize: number = 200;
 }
