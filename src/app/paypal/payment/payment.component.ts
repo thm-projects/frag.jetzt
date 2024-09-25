023a544134166037064dce65d32d788afcc86ac1
MackAlexander <alexander.mack@mni.thm.de>
2024-09-25 10:48:48 +0200

    remove doplicat variables

--- src/app/paypal/payment/payment.component.ts
+++ src/app/paypal/payment/payment.component.ts
@@ -96,8 +96,8 @@ export class PaymentComponent implements OnInit {
   }
 
   // User's token status and whether they have purchased the free plan
-  userTokens = 0; // Example: Number of tokens the user has
-  hasFreePlan = false; // Change this to true if the user has purchased the Free Plan
+  //userTokens = 0; // Example: Number of tokens the user has
+  //hasFreePlan = false; // Change this to true if the user has purchased the Free Plan
 
   ngOnInit() {
     this.loadPayPalScript()
