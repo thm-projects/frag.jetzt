81b56c98bdedbebe7d616ba5416ccebcb9d15c76
MackAlexander <alexander.mack@mni.thm.de>
2024-09-25 17:25:31 +0200

    add disabled plans for mobile devices

--- src/app/paypal/mobile/mobile.component.ts
+++ src/app/paypal/mobile/mobile.component.ts
@@ -13,7 +13,18 @@ import { I18nLoader } from 'app/base/i18n/i18n-loader';
 // Load the i18n data
 const i18n = I18nLoader.load(rawI18n);
 
+// Define PayPal types
+/* interface PayPalData {
+  orderID: string;
+} */
+
 // Define type for pricing plans
+/* interface Plan {
+  title: string;
+  price: string;
+  tokens: number;
+  color: string;
+} */
 
 @Component({
   selector: 'app-mobile',
@@ -35,3 +46,18 @@ export class MobileComponent {
   protected readonly i18n = i18n;
   userTokens = 100;
 }
+
+/*   // Pricing plans
+  plans: Plan[] = [
+    {
+      title: 'Free Plan',
+      price: '0',
+      tokens: 50000,
+      color: 'grey',
+    },
+    { title: '€5 Plan', price: '5', tokens: 50000, color: 'primary' },
+    { title: '€10 Plan', price: '10', tokens: 106000, color: 'primary' },
+    { title: '€20 Plan', price: '20', tokens: 212000, color: 'primary' },
+    { title: '€50 Plan', price: '50', tokens: 530000, color: 'primary' },
+  ];
+ */
