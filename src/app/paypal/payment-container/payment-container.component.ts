8dad4628ee5e34c9e8430379c1d66cd1f84aa8e4
MackAlexander <alexander.mack@mni.thm.de>
2024-09-26 15:08:53 +0200

    add new service and new component for device detection

---
+++ src/app/paypal/payment-container/payment-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-container.component.html',
+  styleUrl: './payment-container.component.scss',
+})
+export class PaymentContainerComponent {}
