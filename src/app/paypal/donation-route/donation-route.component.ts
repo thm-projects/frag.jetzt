4e69cf1126a56785f47c0e3ec266af9390fcb6da
MackAlexander <alexander.mack@mni.thm.de>
2024-09-26 21:30:24 +0200

    add donation component route

--- src/app/paypal/donation-route/donation-route.component.ts
+++ src/app/paypal/donation-route/donation-route.component.ts
@@ -1,10 +1,14 @@
+import { NgComponentOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { donationConfig } from 'environments/paypal.config';
 
 @Component({
   selector: 'app-donation-route',
   standalone: true,
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './donation-route.component.html',
   styleUrl: './donation-route.component.scss',
 })
-export class DonationRouteComponent {}
+export class DonationRouteComponent {
+  readonly donComponent = donationConfig.donation_component;
+}
--- src/app/paypal/payment-route/payment-route.component.ts
+++ src/app/paypal/payment-route/payment-route.component.ts
@@ -1,7 +1,6 @@
 import { NgComponentOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { paymentConfig } from 'environments/paypal.config';
-import { donationConfig } from 'environments/paypal.config';
 
 @Component({
   selector: 'app-payment-route',
@@ -12,8 +11,4 @@ import { donationConfig } from 'environments/paypal.config';
 })
 export class PaymentRouteComponent {
   readonly payComponent = paymentConfig.payment_component;
-}
-
-export class DonationRouteComponent {
-  readonly donComponent = donationConfig.donation_component;
 }
