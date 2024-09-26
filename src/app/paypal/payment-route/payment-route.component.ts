5aeb9576478e13db882489c4820faf3bdc1773c8
MackAlexander <alexander.mack@mni.thm.de>
2024-09-26 21:17:49 +0200

    remove qr-code for mobile devices

--- src/app/paypal/payment-route/payment-route.component.ts
+++ src/app/paypal/payment-route/payment-route.component.ts
@@ -1,6 +1,7 @@
 import { NgComponentOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { paymentConfig } from 'environments/paypal.config';
+import { donationConfig } from 'environments/paypal.config';
 
 @Component({
   selector: 'app-payment-route',
@@ -11,4 +12,8 @@ import { paymentConfig } from 'environments/paypal.config';
 })
 export class PaymentRouteComponent {
   readonly payComponent = paymentConfig.payment_component;
+}
+
+export class DonationRouteComponent {
+  readonly donComponent = donationConfig.donation_component;
 }
--- src/environments/paypal.config.ts
+++ src/environments/paypal.config.ts
@@ -4,8 +4,20 @@ export const paymentConfig = {
   payment_component: PaymentComponent,
 }; */
 
+/* import { DonationComponent } from 'app/paypal/donation/donation.component';
+
+export const donationConfig = {
+  payment_component: DonationComponent,
+
+} */
+
 import { MobileComponent } from 'app/paypal/mobile/mobile.component';
+import { MobiledonationComponent } from 'app/paypal/mobiledonation/mobiledonation.component';
 
 export const paymentConfig = {
   payment_component: MobileComponent,
+};
+
+export const donationConfig = {
+  donation_component: MobiledonationComponent,
 };
