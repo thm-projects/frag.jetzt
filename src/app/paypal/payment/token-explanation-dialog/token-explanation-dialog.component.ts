4e80b6bf6a2e4ef3f84e2842dd7b9f7d0e1cfaf7
Tim Steinmüller <tim.steinmueller@online.de>
2024-09-28 00:32:26 +0200

    fix translation

--- src/app/paypal/payment/token-explanation-dialog/token-explanation-dialog.component.ts
+++ src/app/paypal/payment/token-explanation-dialog/token-explanation-dialog.component.ts
@@ -3,7 +3,13 @@ import { PaymentComponent } from '../payment.component';
 import { CustomMarkdownModule } from 'app/base/custom-markdown/custom-markdown.module';
 import rawI18n from '../i18n.json';
 import { I18nLoader } from 'app/base/i18n/i18n-loader';
-import { MatDialogModule } from '@angular/material/dialog';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
 
 // Load the i18n data
 const i18n = I18nLoader.load(rawI18n);
@@ -11,7 +17,14 @@ const i18n = I18nLoader.load(rawI18n);
 @Component({
   selector: 'app-token-explanation-dialog',
   standalone: true,
-  imports: [MatDialogModule, CustomMarkdownModule],
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+    MatDialogModule,
+    CustomMarkdownModule,
+  ],
   templateUrl: './token-explanation-dialog.component.html',
   styleUrl: './token-explanation-dialog.component.scss',
 })
