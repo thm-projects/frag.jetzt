5c886731288609a4d8b5a943a6ede284001e333c
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-09-27 14:04:57 +0200

    DRY

--- src/app/components/home/_dialogs/cookies/cookies.component.ts
+++ src/app/components/home/_dialogs/cookies/cookies.component.ts
@@ -1,17 +1,14 @@
-import { Component, computed, Signal, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DataProtectionComponent } from '../data-protection/data-protection.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { language } from 'app/base/language/language';
-import { windowWatcher } from 'modules/navigation/utils/window-watcher';
-import { M3WindowSizeClass } from 'modules/m3/components/navigation/m3-navigation-types';
-import { isDark } from '../../../../base/theme/theme';
-
-interface BadgeData {
-  alt: string;
-  src: string;
-}
-
-type ShowBadgeType = 'appstore' | 'playstore' | 'both' | 'none';
+import {
+  appleUrl,
+  badgeType,
+  getAppStoreBadge,
+  getPlayStoreBadge,
+  googleUrl,
+} from '../app-utility';
 
 @Component({
   selector: 'app-cookies',
@@ -21,80 +18,16 @@ type ShowBadgeType = 'appstore' | 'playstore' | 'both' | 'none';
 export class CookiesComponent {
   protected readonly lang = language;
   protected readonly isMobile = signal(false);
-  protected readonly badgeType: Signal<ShowBadgeType> = computed(() => {
-    const isDesktop = windowWatcher.windowState() !== M3WindowSizeClass.Compact;
-    if (isDesktop) {
-      return 'both';
-    }
-    const isInstalled = this.isStandalone();
-    if (isInstalled) {
-      return 'none';
-    }
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    const isAndroid = /Android/i.test(navigator.userAgent);
-    if (isIOS) {
-      return 'appstore';
-    }
-    return isAndroid ? 'playstore' : 'both';
-  });
-  protected readonly googleUrl =
-    'https://play.google.com/store/apps/details?id=com.uninow.thm&hl=de';
-  protected readonly appleUrl =
-    'https://apps.apple.com/us/app/thm-app/id1644060104?itscg=30200&itsct=apps_box_badge&mttnsubad=1644060104';
+  protected readonly badgeType = badgeType;
+  protected readonly getAppStoreBadge = getAppStoreBadge;
+  protected readonly getPlayStoreBadge = getPlayStoreBadge;
+  protected readonly googleUrl = googleUrl;
+  protected readonly appleUrl = appleUrl;
 
   constructor(
     protected dialog: MatDialog,
     private dialogRef: MatDialogRef<CookiesComponent>,
   ) {}
-
-  getAppStoreBadge(badgeType: ShowBadgeType): BadgeData {
-    switch (language()) {
-      case 'de':
-        return {
-          alt: 'Lade die App im App Store herunter',
-          src:
-            isDark() && badgeType === 'appstore'
-              ? 'assets/images/appstore_de_white.svg'
-              : 'assets/images/appstore_de_black.svg',
-        };
-      case 'fr':
-        return {
-          alt: "Télécharge l'application dans l'App Store",
-          src:
-            isDark() && badgeType === 'appstore'
-              ? 'assets/images/appstore_fr_white.svg'
-              : 'assets/images/appstore_fr_black.svg',
-        };
-      default:
-        return {
-          alt: 'Download on the App Store',
-          src:
-            isDark() && badgeType === 'appstore'
-              ? 'assets/images/appstore_en_white.svg'
-              : 'assets/images/appstore_en_black.svg',
-        };
-    }
-  }
-
-  getPlayStoreBadge(): BadgeData {
-    switch (language()) {
-      case 'de':
-        return {
-          alt: 'Lade die App im Play Store herunter',
-          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_German.png',
-        };
-      case 'fr':
-        return {
-          alt: "Télécharge l'application dans l'Play Store",
-          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_French-CA.png',
-        };
-      default:
-        return {
-          alt: 'Get it on Google Play',
-          src: 'assets/images/GetItOnGooglePlay_Badge_Web_color_English.png',
-        };
-    }
-  }
 
   acceptCookies() {
     this.dialogRef.close(true);
@@ -105,12 +38,5 @@ export class CookiesComponent {
 
   openDataProtection() {
     this.dialog.open(DataProtectionComponent);
-  }
-
-  private isStandalone(): boolean {
-    return (
-      navigator['standalone'] ||
-      window.matchMedia('(display-mode: standalone)').matches
-    );
   }
 }
--- src/app/components/home/_dialogs/download/download.component.ts
+++ src/app/components/home/_dialogs/download/download.component.ts
@@ -1,10 +1,10 @@
 import rawI18n from './i18n.json';
-import { Component } from '@angular/core';
-import { CookiesComponent } from '../cookies/cookies.component';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { language } from 'app/base/language/language';
 import { I18nLoader } from 'app/base/i18n/i18n-loader';
 import { ExcuseComponent } from '../excuse/excuse.component';
+import { badgeType, getAppStoreBadge, getPlayStoreBadge } from '../app-utility';
 
 const i18n = I18nLoader.load(rawI18n);
 
@@ -13,19 +13,16 @@ const i18n = I18nLoader.load(rawI18n);
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss'],
 })
-export class DownloadComponent extends CookiesComponent {
-  protected override readonly lang = language;
+export class DownloadComponent {
+  protected readonly lang = language;
   protected readonly i18n = i18n;
+  protected readonly badgeType = badgeType;
+  protected readonly getAppStoreBadge = getAppStoreBadge;
+  protected readonly getPlayStoreBadge = getPlayStoreBadge;
 
-  constructor(
-    dialog: MatDialog, // Inject MatDialog here
-    dialogRef: MatDialogRef<DownloadComponent>,
-  ) {
-    super(dialog, dialogRef); // Pass MatDialog to the parent component
-  }
+  private dialog = inject(MatDialog);
 
-  // Method to show the excuse pop-up
   showExcusePopup() {
-    this.dialog.open(ExcuseComponent, {});
+    this.dialog.open(ExcuseComponent);
   }
 }
