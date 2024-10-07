5c33a1d3ea352430b58dc49ed78fd69765fb71f4
Yousif Wardah <yousif.wardah@mni.thm.de>
2024-10-07 14:01:23 +0200

    Restructure gptchat-room again and add mat drawer

--- src/app/room/gptchat-room/gptchat-room.component.ts
+++ src/app/room/gptchat-room/gptchat-room.component.ts
@@ -197,6 +197,12 @@ export class GPTChatRoomComponent implements OnInit, OnDestroy {
     ];
 
     const threads = this.threads();
+
+    for (const group of groups) {
+      group.threads.push(...threads);
+    }
+
+    /*
     for (let i = threads.length - 1; i >= 0; i--) {
       const thread = threads[i];
       const threadDate = thread.ref.createdAt;
@@ -221,7 +227,7 @@ export class GPTChatRoomComponent implements OnInit, OnDestroy {
       group.threads.sort(
         (a, b) => b.ref.createdAt.getTime() - a.ref.createdAt.getTime(),
       );
-    }
+    }*/
 
     return groups;
   }
