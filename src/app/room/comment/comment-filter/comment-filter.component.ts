40a4c0fd2210de1bab1d1ec607bc0df9510b3c04
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-10-23 10:53:42 +0200

    Fix bug when filtering

--- src/app/room/comment/comment-filter/comment-filter.component.ts
+++ src/app/room/comment/comment-filter/comment-filter.component.ts
@@ -1,7 +1,6 @@
 import { Component, effect, input, output, signal } from '@angular/core';
 import { ForumComment } from 'app/utils/data-accessor';
 import { Filter } from '../comment/comment.component';
-import { user } from 'app/user/state/user';
 import { commentsMeta } from 'app/room/state/comments';
 
 @Component({
@@ -13,7 +12,6 @@ export class CommentFilterComponent {
   comment = input.required<ForumComment>();
   filterSelect = output<Filter>();
   protected readonly userCount = signal(0);
-  protected readonly user = user;
 
   constructor() {
     effect(
