f70437019fe31d618c8a0521998fb11ef83def3d
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-11-23 16:03:06 +0100

    Finish restriction setting

--- src/app/room/assistant-route/restrictions-manage/restrictions-manage.component.ts
+++ src/app/room/assistant-route/restrictions-manage/restrictions-manage.component.ts
@@ -6,11 +6,16 @@ import {
   computed,
   effect,
   inject,
+  input,
   Signal,
   signal,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
@@ -25,6 +30,21 @@ import { DateAdapter, provideNativeDateAdapter } from '@angular/material/core';
 import { language } from 'app/base/language/language';
 import { ContextPipe } from 'app/base/i18n/context.pipe';
 import { i18nContext } from 'app/base/i18n/i18n-context';
+import { UUID } from 'app/utils/ts-utils';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {
+  AssistantRestrictionService,
+  BlockRestriction,
+  InputBlockRestriction,
+  InputQuotaRestriction,
+  InputTimeRestriction,
+  QuotaRestriction,
+  TimeRestriction as TR,
+} from '../services/assistant-restriction.service';
+import { concatMap, forkJoin, map, Observable, of } from 'rxjs';
+import { room } from 'app/room/state/room';
+import { user } from 'app/user/state/user';
+import { NotificationService } from 'app/services/util/notification.service';
 
 const TARGETS = [
   'ALL',
@@ -71,6 +91,7 @@ interface Quota {
   };
   last_reset: Date;
   end_time: Date;
+  timezone: string;
 }
 
 interface TimeRestriction {
@@ -101,12 +122,15 @@ interface TimeRestriction {
     MatTimepickerModule,
     MatDatepickerModule,
     ContextPipe,
+    MatProgressSpinnerModule,
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './restrictions-manage.component.html',
   styleUrl: './restrictions-manage.component.scss',
 })
 export class RestrictionsManageComponent {
+  readonly restrictionsId = input(null as UUID | null);
+  readonly mode = input.required<'room' | 'user'>();
   readonly i18n = i18n;
   readonly TARGETS = TARGETS;
   protected readonly currentDate = new Date();
@@ -155,6 +179,13 @@ export class RestrictionsManageComponent {
     multiplier: [1, [Validators.required, Validators.min(1)]],
   });
   protected readonly times = signal([] as TimeRestriction[]);
+  protected readonly saving = signal(false);
+  private previousQuotas: QuotaRestriction[] = [];
+  private previousBlocks: BlockRestriction[] = [];
+  private previousTimes: TR[] = [];
+  private readonly restriction = inject(AssistantRestrictionService);
+  private readonly notify = inject(NotificationService);
+  private readonly dialogRef = inject(MatDialogRef);
   private readonly _adapter =
     inject<DateAdapter<unknown, unknown>>(DateAdapter);
 
@@ -162,14 +193,32 @@ export class RestrictionsManageComponent {
     effect(() => {
       this._adapter.setLocale(language());
     });
+    effect(() => {
+      const id = this.restrictionsId();
+      this.times.set([]);
+      this.quotas.set([]);
+      this.blockedTargets.set([]);
+      this.previousBlocks = null;
+      this.previousQuotas = null;
+      this.previousTimes = null;
+      if (id) {
+        this.loadRestrictions(id);
+      }
+    });
   }
 
-  static open(dialog: MatDialog) {
+  static open(
+    dialog: MatDialog,
+    restrictionsId: UUID | null,
+    mode: 'room' | 'user',
+  ) {
     const ref = dialog.open(RestrictionsManageComponent, {
       width: '100%',
       height: '100%',
       panelClass: 'full-screen-dialog',
     });
+    ref.componentRef.setInput('restrictionsId', restrictionsId);
+    ref.componentRef.setInput('mode', mode);
     return ref;
   }
 
@@ -220,7 +269,7 @@ export class RestrictionsManageComponent {
         strategy: this.inputTime.value.strategy,
         multiplier: this.inputTime.value.multiplier,
       },
-      timezone: 'UTC', //this.currentTimezone,
+      timezone: this.currentTimezone,
     };
     this.inputTime.reset({
       startTime: new Date(),
@@ -249,6 +298,7 @@ export class RestrictionsManageComponent {
       },
       last_reset: this.inputQuota.value.lastReset,
       end_time: this.inputQuota.value.endTime,
+      timezone: this.currentTimezone,
     };
     this.inputQuota.reset({
       quota: 0,
@@ -353,6 +403,25 @@ export class RestrictionsManageComponent {
     }
   }
 
+  protected save() {
+    if (this.saving()) {
+      return;
+    }
+    this.saving.set(true);
+    this.saveRestrictions().subscribe({
+      next: (id) => {
+        this.saving.set(false);
+        this.dialogRef.close(id);
+        this.notify.show(i18n().global.changeSuccessful);
+      },
+      error: (e) => {
+        console.error(e);
+        this.saving.set(false);
+        this.notify.show(i18n().global.changesGoneWrong);
+      },
+    });
+  }
+
   private filterBlock(): RestrictionTarget[] {
     const blocked = this.blockedTargets();
     const set = new Set(blocked.map((b) => b));
@@ -360,5 +429,284 @@ export class RestrictionsManageComponent {
       return [];
     }
     return TARGETS.filter((v) => FILTER_OBJECT[v](set));
+  }
+
+  private parseRepeatStrategy(strategy: string): Quota['reset_strategy'] {
+    const regex = /^(\d+)(d|w|m|y)$/;
+    const match = strategy.match(regex);
+    if (!match) {
+      return { strategy: 'never', multiplier: 1 };
+    }
+    let strategyValue: 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+    switch (match[2]) {
+      case 'd':
+        strategyValue = 'daily';
+        break;
+      case 'w':
+        strategyValue = 'weekly';
+        break;
+      case 'm':
+        strategyValue = 'monthly';
+        break;
+      case 'y':
+        strategyValue = 'yearly';
+        break;
+      default:
+        strategyValue = 'never';
+    }
+    return { strategy: strategyValue, multiplier: Number(match[1]) };
+  }
+
+  private loadRestrictions(id: UUID) {
+    const blockObservable: Observable<BlockRestriction[]> =
+      this.mode() === 'room'
+        ? this.restriction.listBlockRestrictionsForRoom(id)
+        : this.restriction.listBlockRestrictionsForUser(id);
+    blockObservable.subscribe((blocks) => {
+      this.previousBlocks = blocks;
+      this.blockedTargets.set(blocks.map((b) => b.target));
+    });
+    const quotaObservable: Observable<QuotaRestriction[]> =
+      this.mode() === 'room'
+        ? this.restriction.listQuotaRestrictionsForRoom(id)
+        : this.restriction.listQuotaRestrictionsForUser(id);
+    quotaObservable.subscribe((quotas) => {
+      this.previousQuotas = quotas;
+      this.quotas.set(
+        quotas.map(
+          (q) =>
+            ({
+              quota: Number(q.quota),
+              target: q.target,
+              reset_strategy: this.parseRepeatStrategy(q.reset_strategy),
+              last_reset: new Date(q.last_reset),
+              end_time: new Date(q.end_time),
+              timezone: q.timezone,
+            }) satisfies Quota,
+        ),
+      );
+    });
+    const timeObservable: Observable<TR[]> =
+      this.mode() === 'room'
+        ? this.restriction.listTimeRestrictionsForRoom(id)
+        : this.restriction.listTimeRestrictionsForUser(id);
+    timeObservable.subscribe((times) => {
+      this.previousTimes = times;
+      this.times.set(
+        times.map(
+          (t) =>
+            ({
+              start_time: new Date(t.start_time),
+              end_time: new Date(t.end_time),
+              target: t.target,
+              repeat_strategy: this.parseRepeatStrategy(t.repeat_strategy),
+              timezone: t.timezone,
+            }) satisfies TimeRestriction,
+        ),
+      );
+    });
+  }
+
+  private saveRestrictions() {
+    const id = this.restrictionsId();
+    let start = of(id);
+    if (!id) {
+      start = (
+        this.mode() === 'room'
+          ? this.restriction.createRoomRestriction(room().id)
+          : this.restriction.createUserRestriction(user().id)
+      ).pipe(map((r) => r.id));
+    }
+    return start.pipe(
+      concatMap((restriction_id) =>
+        forkJoin([
+          this.saveBlocks(restriction_id),
+          this.saveQuotas(restriction_id),
+          this.saveTimes(restriction_id),
+        ]).pipe(map(() => restriction_id)),
+      ),
+    );
+  }
+
+  private saveBlocks(restriction_id: UUID) {
+    const previousBlocked = new Set(this.previousBlocks?.map((b) => b.target));
+    const toCreate: InputBlockRestriction[] = [];
+    for (const block of this.blockedTargets()) {
+      if (previousBlocked.delete(block)) {
+        // Already present
+        continue;
+      }
+      // new block
+      toCreate.push({ target: block, restriction_id });
+    }
+    const toDelete =
+      this.previousBlocks?.filter((b) => previousBlocked.has(b.target)) ?? [];
+    // patching
+    let start: Observable<unknown> = of(null);
+    if (toDelete.length > 0) {
+      start = forkJoin(
+        toDelete.map((block) =>
+          this.mode() === 'room'
+            ? this.restriction.deleteBlockRestrictionForRoom(
+                restriction_id,
+                block.id,
+              )
+            : this.restriction.deleteBlockRestrictionForUser(
+                restriction_id,
+                block.id,
+              ),
+        ),
+      );
+    }
+    if (toCreate.length > 0) {
+      start = start.pipe(
+        concatMap(() =>
+          forkJoin(
+            toCreate.map((block) =>
+              this.mode() === 'room'
+                ? this.restriction.createBlockRestrictionForRoom(block)
+                : this.restriction.createBlockRestrictionForUser(block),
+            ),
+          ),
+        ),
+      );
+    }
+    return start;
+  }
+
+  private areQuotasEqual(a: QuotaRestriction, b: Quota): boolean {
+    return (
+      a.quota === String(b.quota) &&
+      a.target === b.target &&
+      a.reset_strategy ===
+        b.reset_strategy.multiplier + b.reset_strategy.strategy[0] &&
+      a.last_reset === b.last_reset &&
+      a.end_time === b.end_time &&
+      a.timezone === b.timezone
+    );
+  }
+
+  private saveQuotas(restriction_id: UUID) {
+    let previousQuotas = this.previousQuotas ?? [];
+    const toCreate: InputQuotaRestriction[] = [];
+    for (const quota of this.quotas()) {
+      const before = previousQuotas.length;
+      previousQuotas = previousQuotas.filter((q) =>
+        this.areQuotasEqual(q, quota),
+      );
+      if (before !== previousQuotas.length) {
+        // Already present
+        continue;
+      }
+      // new quota
+      toCreate.push({
+        target: quota.target,
+        quota: String(quota.quota),
+        reset_strategy:
+          quota.reset_strategy.multiplier + quota.reset_strategy.strategy[0],
+        last_reset: quota.last_reset,
+        end_time: quota.end_time,
+        restriction_id,
+        timezone: this.currentTimezone,
+      });
+    }
+    const toDelete = previousQuotas;
+    // patching
+    let start: Observable<unknown> = of(null);
+    if (toDelete.length > 0) {
+      start = forkJoin(
+        toDelete.map((quota) =>
+          this.mode() === 'room'
+            ? this.restriction.deleteQuotaRestrictionForRoom(
+                restriction_id,
+                quota.id,
+              )
+            : this.restriction.deleteQuotaRestrictionForUser(
+                restriction_id,
+                quota.id,
+              ),
+        ),
+      );
+    }
+    if (toCreate.length > 0) {
+      start = start.pipe(
+        concatMap(() =>
+          forkJoin(
+            toCreate.map((quota) =>
+              this.mode() === 'room'
+                ? this.restriction.createQuotaRestrictionForRoom(quota)
+                : this.restriction.createQuotaRestrictionForUser(quota),
+            ),
+          ),
+        ),
+      );
+    }
+    return start;
+  }
+
+  private areTimesEqual(a: TR, b: TimeRestriction): boolean {
+    return (
+      a.start_time === b.start_time &&
+      a.end_time === b.end_time &&
+      a.target === b.target &&
+      a.repeat_strategy ===
+        b.repeat_strategy.multiplier + b.repeat_strategy.strategy[0] &&
+      a.timezone === b.timezone
+    );
+  }
+
+  private saveTimes(restriction_id: UUID) {
+    let previousTimes = this.previousTimes ?? [];
+    const toCreate: InputTimeRestriction[] = [];
+    for (const time of this.times()) {
+      const before = previousTimes.length;
+      previousTimes = previousTimes.filter((t) => this.areTimesEqual(t, time));
+      if (before !== previousTimes.length) {
+        // Already present
+        continue;
+      }
+      // new time
+      toCreate.push({
+        restriction_id,
+        start_time: time.start_time,
+        end_time: time.end_time,
+        target: time.target,
+        repeat_strategy:
+          time.repeat_strategy.multiplier + time.repeat_strategy.strategy[0],
+        timezone: time.timezone,
+      });
+    }
+    const toDelete = previousTimes;
+    // patching
+    let start: Observable<unknown> = of(null);
+    if (toDelete.length > 0) {
+      start = forkJoin(
+        toDelete.map((time) =>
+          this.mode() === 'room'
+            ? this.restriction.deleteTimeRestrictionForRoom(
+                restriction_id,
+                time.id,
+              )
+            : this.restriction.deleteTimeRestrictionForUser(
+                restriction_id,
+                time.id,
+              ),
+        ),
+      );
+    }
+    if (toCreate.length > 0) {
+      start = start.pipe(
+        concatMap(() =>
+          forkJoin(
+            toCreate.map((time) =>
+              this.mode() === 'room'
+                ? this.restriction.createTimeRestrictionForRoom(time)
+                : this.restriction.createTimeRestrictionForUser(time),
+            ),
+          ),
+        ),
+      );
+    }
+    return start;
   }
 }
