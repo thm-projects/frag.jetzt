fc474d26d7f84c14c0048fa6060abdb2db78f201
Ruben Bimberg <ruben.bimberg@mni.thm.de>
2024-12-19 13:28:35 +0100

    Fix comment edit

--- src/app/components/shared/_dialogs/edit-question/edit-question.component.ts
+++ src/app/components/shared/_dialogs/edit-question/edit-question.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Comment } from 'app/models/comment';
 import { CommentService } from 'app/services/http/comment.service';
@@ -11,7 +11,7 @@ import { TSMap } from 'typescript-map';
   standalone: false,
 })
 export class EditQuestionComponent {
-  comment: Comment;
+  comment = input<Comment>(null);
 
   constructor(
     private ref: MatDialogRef<EditQuestionComponent>,
@@ -34,28 +34,28 @@ export class EditQuestionComponent {
     }
     const changes = new TSMap<keyof Comment, unknown>();
     const newBody = c.body;
-    if (newBody !== this.comment.body) {
+    if (newBody !== this.comment().body) {
       changes.set('body', newBody);
     }
-    if (c.language !== this.comment.language) {
+    if (c.language !== this.comment().language) {
       changes.set('language', c.language);
     }
     const newKeyQuestioner = JSON.stringify(c.keywordsFromQuestioner);
-    if (c.tag !== this.comment.tag) {
+    if (c.tag !== this.comment().tag) {
       changes.set('tag', c.tag);
     }
     if (
-      newKeyQuestioner !== JSON.stringify(this.comment.keywordsFromQuestioner)
+      newKeyQuestioner !== JSON.stringify(this.comment().keywordsFromQuestioner)
     ) {
       changes.set('keywordsFromQuestioner', newKeyQuestioner);
     }
     const newKeySpaCy = JSON.stringify(c.keywordsFromSpacy);
-    if (newKeyQuestioner !== JSON.stringify(this.comment.keywordsFromSpacy)) {
+    if (newKeyQuestioner !== JSON.stringify(this.comment().keywordsFromSpacy)) {
       changes.set('keywordsFromSpacy', newKeySpaCy);
     }
     if (changes.size() === 0) {
       return;
     }
-    this.commentService.patchComment(this.comment, changes).subscribe();
+    this.commentService.patchComment(this.comment(), changes).subscribe();
   }
 }
