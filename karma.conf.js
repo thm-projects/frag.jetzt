0ad69549ca9e8a1b2dbf6f8ace82c6800c951ff2
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel.ext@iu.org>
2025-04-25 11:44:39 +0200

    Enhance test configuration: enable code coverage, improve CI settings, and streamline Karma setup

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = async function (config) {
+module.exports = async function(config) {
   const isDocker = await import('is-docker');
+  
+  // First, detect CI environment
+  const isCi = process.env.CI === 'true';
+  
   config.set({
     basePath: '',
-    browsers: ['ChromeHeadlessCustom', 'Chrome'],
+    browsers: ['ChromeHeadlessCustom'],
     customLaunchers: {
       ChromeHeadlessCustom: {
         base: 'ChromeHeadless',
@@ -21,12 +25,17 @@ module.exports = async function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false, // Run tests in order they are defined
+        failFast: false // Don't stop on first failure
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
+      verbose: false,
       thresholds: {
         emitWarning: false,
         each: {
@@ -38,12 +47,22 @@ module.exports = async function (config) {
       }
     },
     reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-    // Use angular.json for additional files
+    reportSlowerThan: 500,
     files: [],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    singleRun: false
+    singleRun: false,
+    
+    // Add reasonable timeouts for CI environments
+    browserDisconnectTimeout: 10000,
+    browserNoActivityTimeout: 60000,
+    captureTimeout: 60000
   });
+
+  // Only add Chrome for local development
+  if (!isCi) {
+    config.browsers.push('Chrome');
+  }
 };
