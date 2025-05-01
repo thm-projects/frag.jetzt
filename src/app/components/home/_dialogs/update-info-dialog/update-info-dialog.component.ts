5650e64e85c3f45c71d54a99ce6c66dc926c65c9
Klaus Quibeldey-Cirkel <klaus.quibeldey-cirkel.ext@iu.org>
2025-05-01 10:40:04 +0200

    feat: enhance dialog title management by adding support for update info dialog and improving translation keys

--- src/app/components/home/_dialogs/update-info-dialog/update-info-dialog.component.ts
+++ src/app/components/home/_dialogs/update-info-dialog/update-info-dialog.component.ts
@@ -1,7 +1,7 @@
 import rawI18n from './i18n.json';
 import { I18nLoader } from 'app/base/i18n/i18n-loader';
 const i18n = I18nLoader.load(rawI18n);
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   MatDialog,
   MatDialogModule,
@@ -9,6 +9,7 @@ import {
 } from '@angular/material/dialog';
 import { CustomMarkdownModule } from 'app/base/custom-markdown/custom-markdown.module';
 import { MatButtonModule } from '@angular/material/button';
+import { AppTitleStrategy } from 'app/services/title/app-title-strategy';
 
 @Component({
   selector: 'app-update-info-dialog',
@@ -16,10 +17,23 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./update-info-dialog.component.scss'],
   imports: [CustomMarkdownModule, MatDialogModule, MatButtonModule],
 })
-export class UpdateInfoDialogComponent {
+export class UpdateInfoDialogComponent implements OnInit, OnDestroy {
   protected readonly i18n = i18n;
 
-  constructor(private dialogRef: MatDialogRef<UpdateInfoDialogComponent>) {}
+  constructor(
+    private readonly dialogRef: MatDialogRef<UpdateInfoDialogComponent>,
+    private readonly titleStrategy: AppTitleStrategy,
+  ) {}
+
+  ngOnInit(): void {
+    // Set the dialog title when the dialog is opened
+    this.titleStrategy.setDialogTitle('UPDATE_INFO_DIALOG');
+  }
+
+  ngOnDestroy(): void {
+    // Restore the original title when the dialog is closed
+    this.titleStrategy.restoreOriginalTitle();
+  }
 
   public static open(dialog: MatDialog) {
     const ref = dialog.open(UpdateInfoDialogComponent, {
